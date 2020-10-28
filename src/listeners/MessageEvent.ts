b73087927a144e29409479138c510faff0047c29
mozartrafi <mozartrafikt@gmail.com>
2020-10-28 21:38:14 +0700

    fix(MessageEvent): ===/==

--- src/listeners/MessageEvent.ts
+++ src/listeners/MessageEvent.ts
@@ -9,7 +9,7 @@ export default class MessageEvent implements ClientEventListener {
     public execute(message: IMessage): any {
         if (message.author.bot) return message;
         if (message.channel.type === "dm") return message;
-        if (message.content == `<@${this.client.user!.id}>` && message.content == `<@!${this.client.user!.id}>`) {
+        if (message.content === `<@${this.client.user!.id}>` && message.content === `<@!${this.client.user!.id}>`) {
             return message.channel.send(
                 new MessageEmbed().setDescription(`Hi, my prefix is **\`${this.client.config.prefix}\`**`).setColor(this.client.config.embedColor)
             );
