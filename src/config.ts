4be636eb07a16d830e3ec0da57ef202d4d24a1e2
mozartrafi <mozartrafikt@gmail.com>
2020-11-01 20:21:04 +0700

    fix(config): forget to define discord.js AcitivityType

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,5 @@
+import type { ActivityType } from "discord.js";
+
 export const name: string = process.env.CONFIG_NAME ?? "Disc 11";
 export const prefix = process.env.CONFIG_PREFIX?.replace(/"/g, "") ?? "!"; // Temporary workaround for https://github.com/docker/compose/issues/6951
 export const embedColor = process.env.CONFIG_EMBED_COLOR ?? "7289DA";
