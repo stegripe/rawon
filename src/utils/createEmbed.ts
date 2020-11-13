1a4481b35ca422c07f48bc8be1ce26ff330f475c
mozartrafi <mozartrafikt@gmail.com>
2020-11-13 13:59:13 +0700

    fix(createEmbed): move hexColors variable out of createEmbed function

--- src/utils/createEmbed.ts
+++ src/utils/createEmbed.ts
@@ -1,12 +1,14 @@
 import { MessageEmbed } from "discord.js";
 import { embedColor } from "../config";
 
-export function createEmbed(type: "info" | "warn" | "error", message?: string): MessageEmbed {
-    const hexColors = {
-        info: embedColor,
-        warn: "YELLOW",
-        error: "RED"
-    };
+type hexColorsType = "info" | "warn" | "error";
+const hexColors: Record<hexColorsType, string> = {
+    info: embedColor,
+    warn: "#FFFF00",
+    error: "#FF0000"
+};
+
+export function createEmbed(type: hexColorsType, message?: string): MessageEmbed {
     const embed = new MessageEmbed()
         .setColor(hexColors[type]);
 
