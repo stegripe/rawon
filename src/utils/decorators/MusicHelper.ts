156695464a38cab57d1282f337a7c4b55c216fe5
mozartrafi <mozartrafikt@gmail.com>
2020-11-23 21:23:38 +0700

    fix(MusicHelper): fix bot doesn't know where he is

--- src/utils/decorators/MusicHelper.ts
+++ src/utils/decorators/MusicHelper.ts
@@ -24,7 +24,8 @@ export function isMusicPlaying(): any {
 export function isSameVoiceChannel(): any {
     return inhibit(message => {
         if (!message.guild?.me?.voice.channel) return undefined;
-        if (message.member?.voice.channel?.id !== message.guild.queue?.voiceChannel?.id) {
+        const botVoiceChannel = message.guild.queue?.voiceChannel?.id ?? message.guild.me.voice.channel.id;
+        if (message.member?.voice.channel?.id !== botVoiceChannel) {
             return message.channel.send(
                 createEmbed("warn", "You need to be in a same voice channel as mine")
             );
