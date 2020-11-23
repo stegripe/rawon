d2489b3ad5240c6b62cbc6bf5eb8059d63191578
mozartrafi <mozartrafikt@gmail.com>
2020-11-23 22:08:48 +0700

    fix(MessageEvent): use regex to fix mention detection

--- src/listeners/MessageEvent.ts
+++ src/listeners/MessageEvent.ts
@@ -2,18 +2,27 @@ import { IMessage } from "../../typings";
 import { DefineListener } from "../utils/decorators/DefineListener";
 import { createEmbed } from "../utils/createEmbed";
 import { BaseListener } from "../structures/BaseListener";
+import { User } from "discord.js";
 
 @DefineListener("message")
 export class MessageEvent extends BaseListener {
-    public execute(message: IMessage): any {
+    public async execute(message: IMessage): Promise<any> {
         if (message.author.bot) return message;
         if (message.channel.type === "dm") return message;
-        if (message.content === message.guild?.me?.toString()) {
+        if ((await this.getUserFromMention(message.content))?.id === message.client.user?.id) {
             return message.channel.send(
                 createEmbed("info", `👋  **|**  Hi there, my prefix is **\`${this.client.config.prefix}\`**`)
             );
         }
         if (!message.content.toLowerCase().startsWith(this.client.config.prefix)) return message;
         return this.client.commands.handle(message);
+    }
+
+    private getUserFromMention(mention: string): Promise<User | undefined> {
+        const matches = /^<@!?(\d+)>$/.exec(mention);
+        if (!matches) return Promise.resolve(undefined);
+
+        const id = matches[1];
+        return this.client.users.fetch(id);
     }
 }
