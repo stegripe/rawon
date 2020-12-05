790141a82ea3db6ba8aa19d553ad39322897fee9
zarrttt <undefinedgaming01@gmail.com>
2020-12-05 20:09:46 +0700

    fix(Sharding): fix Shards are still being spawned error on startup

--- src/listeners/ReadyEvent.ts
+++ src/listeners/ReadyEvent.ts
@@ -4,7 +4,11 @@ import { DefineListener } from "../utils/decorators/DefineListener";
 @DefineListener("ready")
 export class ReadyEvent extends BaseListener {
     public execute(): void {
-        this.client.logger.info(`${this.client.shard ? `[Shard #${this.client.shard.ids[0]}]` : ""} ${this.client.user!.tag} is ready to serve ${this.client.guilds.cache.size} guilds!`);
+        this.client.logger.info(
+            `${this.client.shard ? `[Shard #${this.client.shard.ids[0]}]` : ""} I'm ready to serve ${this.client.guilds.cache.size} guilds ` +
+            `with ${this.client.channels.cache.filter(c => c.type === "text").size} text channels and ` +
+            `${this.client.channels.cache.filter(c => c.type === "voice").size} voice channels`
+        );
         const updatePresence = async (): Promise<void> => {
             const activityName = this.client.config.status.activity
                 .replace(/{guildsCount}/g, (await this.client.getGuildsCount()).toString())
@@ -15,6 +19,8 @@ export class ReadyEvent extends BaseListener {
                 activity: { name: activityName, type: this.client.config.status.type }
             }).catch(e => this.client.logger.error("CLIENT_UPDATE_PRESENCE_ERR:", e));
         };
-        setInterval(updatePresence, 30 * 1000); void updatePresence();
+        updatePresence()
+            .then(() => setInterval(updatePresence, 30 * 1000))
+            .catch(e => { if (e.message !== "Shards are still being spawned.") this.client.logger.error(e); });
     }
 }
