8bddf14cacd184c17a06f928564d4fd57687fbe1
zarrttt <undefinedgaming01@gmail.com>
2020-12-05 20:22:42 +0700

    refactor(ReadyEvent): make updatePresence function a class method

--- src/listeners/ReadyEvent.ts
+++ src/listeners/ReadyEvent.ts
@@ -1,3 +1,4 @@
+import { Presence } from "discord.js";
 import { BaseListener } from "../structures/BaseListener";
 import { DefineListener } from "../utils/decorators/DefineListener";
 
@@ -9,18 +10,19 @@ export class ReadyEvent extends BaseListener {
             `with ${this.client.channels.cache.filter(c => c.type === "text").size} text channels and ` +
             `${this.client.channels.cache.filter(c => c.type === "voice").size} voice channels`
         );
-        const updatePresence = async (): Promise<void> => {
-            const activityName = this.client.config.status.activity
-                .replace(/{guildsCount}/g, (await this.client.getGuildsCount()).toString())
-                .replace(/{playingCount}/g, (await this.client.getTotalPlaying()).toString())
-                .replace(/{usersCount}/g, (await this.client.getUsersCount()).toString())
-                .replace(/{botPrefix}/g, this.client.config.prefix);
-            this.client.user?.setPresence({
-                activity: { name: activityName, type: this.client.config.status.type }
-            }).catch(e => this.client.logger.error("CLIENT_UPDATE_PRESENCE_ERR:", e));
-        };
-        updatePresence()
-            .then(() => setInterval(() => updatePresence(), 30 * 1000))
+        this.updatePresence()
+            .then(() => setInterval(() => this.updatePresence(), 30 * 1000))
             .catch(e => { if (e.message !== "Shards are still being spawned.") this.client.logger.error(e); });
+    }
+
+    private async updatePresence(): Promise<Presence | undefined> {
+        const activityName = this.client.config.status.activity
+            .replace(/{guildsCount}/g, (await this.client.getGuildsCount()).toString())
+            .replace(/{playingCount}/g, (await this.client.getTotalPlaying()).toString())
+            .replace(/{usersCount}/g, (await this.client.getUsersCount()).toString())
+            .replace(/{botPrefix}/g, this.client.config.prefix);
+        return this.client.user?.setPresence({
+            activity: { name: activityName, type: this.client.config.status.type }
+        }).catch(e => { this.client.logger.error("CLIENT_UPDATE_PRESENCE_ERR:", e); return undefined; });
     }
 }
