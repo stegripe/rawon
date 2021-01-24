02dbf2bb669736a8922aaf6849c9de4e9f60ae2b
zarrttt <undefinedgaming01@gmail.com>
2021-01-24 10:24:43 +0700

    fix(ListenerLoader): update bacotan

--- src/utils/ListenerLoader.ts
+++ src/utils/ListenerLoader.ts
@@ -12,9 +12,9 @@ export class ListenerLoader {
             const event = await this.import(resolve(this.path, file), this.client);
             if (event === undefined) throw new Error(`File ${file} is not a valid listener file`);
             this.client.on(event.name, (...args) => event.execute(...args));
-            this.client.logger.info(`${this.client.shard ? `[Shard #${this.client.shard.ids[0]}]` : ""} Listener for event ${event.name} has been loaded!`);
+            this.client.logger.info(`${this.client.shard ? `[Shard #${this.client.shard.ids[0]}]` : ""} Listener for event ${event.name} has been loaded.`);
         }
-        this.client.logger.info(`${this.client.shard ? `[Shard #${this.client.shard.ids[0]}]` : ""} A total of ${files.length} of listeners has been loaded!`);
+        this.client.logger.info(`${this.client.shard ? `[Shard #${this.client.shard.ids[0]}]` : ""} A total of ${files.length} of listeners has been loaded.`);
         return this.client;
     }
 
