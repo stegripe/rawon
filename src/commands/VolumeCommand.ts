fadacea9a9311fa799670dd7eeebb88b1e74ebee
zarrttt <undefinedgaming01@gmail.com>
2021-01-27 18:47:12 +0700

    fix(VolumeCommand): update bacotan

--- src/commands/VolumeCommand.ts
+++ src/commands/VolumeCommand.ts
@@ -7,7 +7,7 @@ import { createEmbed } from "../utils/createEmbed";
 @DefineCommand({
     aliases: ["vol", "v"],
     name: "volume",
-    description: "Show or set the track volume",
+    description: "Show or set the music player volume",
     usage: "{prefix}volume [level]"
 })
 export class VolumeCommand extends BaseCommand {
@@ -17,13 +17,13 @@ export class VolumeCommand extends BaseCommand {
     public execute(message: IMessage, args: string[]): any {
         let volume = Number(args[0]);
 
-        if (isNaN(volume)) return message.channel.send(createEmbed("info", `🔊  **|**  The current volume is **\`${message.guild!.queue!.volume.toString()}\`**`));
+        if (isNaN(volume)) return message.channel.send(createEmbed("info", `🔊  **|**  The music player volume is **\`${message.guild!.queue!.volume.toString()}\`**`));
 
         if (volume < 0) volume = 0;
-        if (volume === 0) return message.channel.send(createEmbed("warn", "Please pause the music instead of setting the volume to **\`0\`**"));
+        if (volume === 0) return message.channel.send(createEmbed("warn", "Please pause the music player instead of setting the volume to **\`0\`**"));
         if (Number(args[0]) > this.client.config.maxVolume) {
             return message.channel.send(
-                createEmbed("warn", `You cannot set the volume above **\`${this.client.config.maxVolume}\`**`)
+                createEmbed("error", `You cannot set the volume above **\`${this.client.config.maxVolume}\`**`)
             );
         }
 
