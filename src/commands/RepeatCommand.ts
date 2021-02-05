bcf52cb55aa3b58a8aca372cfa0780a6ab05f46f
zarrttt <undefinedgaming01@gmail.com>
2021-02-05 22:52:07 +0700

    refactor(RepeatCommand): Refactor repeat command and allow user to get current repeat mode

--- src/commands/RepeatCommand.ts
+++ src/commands/RepeatCommand.ts
@@ -1,3 +1,4 @@
+/* eslint-disable sort-keys */
 import { BaseCommand } from "../structures/BaseCommand";
 import { IMessage } from "../../typings";
 import { DefineCommand } from "../utils/decorators/DefineCommand";
@@ -8,24 +9,42 @@ import { createEmbed } from "../utils/createEmbed";
     aliases: ["loop", "music-repeat", "music-loop"],
     name: "repeat",
     description: "Repeat the music player in the queue",
-    usage: "{prefix}repeat <all|one|disable>"
+    usage: "{prefix}repeat [all|one|disable]"
 })
 export class RepeatCommand extends BaseCommand {
     @isUserInTheVoiceChannel()
     @isMusicPlaying()
     @isSameVoiceChannel()
     public execute(message: IMessage, args: string[]): any {
-        const mode = args[0];
-        if (mode === "all" || mode === "queue" || mode === "*" || mode === "2") {
-            message.guild!.queue!.loopMode = 2;
-            return message.channel.send(createEmbed("info", "🔁  **|**  Repeating all music in the queue"));
-        } else if (mode === "current" || mode === "one" || mode === "musiconly" || mode === "1") {
-            message.guild!.queue!.loopMode = 1;
-            return message.channel.send(createEmbed("info", "🔂  **|**  Repeating this music only"));
-        } else if (mode === "disable" || mode === "off" || mode === "0") {
-            message.guild!.queue!.loopMode = 0;
-            return message.channel.send(createEmbed("info", "▶  **|**  Repeating disabled"));
+        const modes: Record<any, 0 | 1 | 2> = {
+            // Repeat All Music in Queue
+            all: 2,
+            queue: 2,
+            "*": 2,
+            2: 2,
+            // Repeat current music
+            current: 1,
+            one: 1,
+            musiconly: 1,
+            1: 1,
+            // Disable repeat
+            disable: 0,
+            off: 0,
+            0: 0
+        };
+        const modeTypes = ["disabled", "only this music", "all items in the queue"];
+        const modeEmoji = ["▶  **|** ", "🔂  **|** ", "🔁  **|** "];
+        const mode = args[0] as string | undefined;
+        if (mode === undefined) {
+            message.channel.send(createEmbed("info", `${modeEmoji[message.guild!.queue!.loopMode]} Current repeat mode: **\`${modeTypes[message.guild!.queue!.loopMode]}\`**`))
+                .catch(e => this.client.logger.error("REPEAT_CMD_ERR:", e));
+        } else if (Object.keys(modes).includes(mode)) {
+            message.guild!.queue!.loopMode = modes[mode];
+            message.channel.send(createEmbed("info", `${modeEmoji[message.guild!.queue!.loopMode]} I set the repeat mode to: **\`${modeTypes[message.guild!.queue!.loopMode]}\`**`))
+                .catch(e => this.client.logger.error("REPEAT_CMD_ERR:", e));
+        } else {
+            message.channel.send(createEmbed("error", `Invalid usage, see **\`${this.client.config.prefix}help ${this.meta.name}\`** for more information`))
+                .catch(e => this.client.logger.error("REPEAT_CMD_ERR:", e));
         }
-        message.channel.send(createEmbed("error", `Invalid usage, see **\`${this.client.config.prefix}help ${this.meta.name}\`** for more information`)).catch(e => this.client.logger.error("REPEAT_CMD_ERR:", e));
     }
 }
