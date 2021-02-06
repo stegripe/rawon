c8b8122d97572c6160d5838219b2bef756a96719
zarrttt <undefinedgaming01@gmail.com>
2021-02-06 14:06:54 +0700

    feat(ServerQueue): use setter & getter for playing property

--- src/structures/ServerQueue.ts
+++ src/structures/ServerQueue.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-underscore-dangle, @typescript-eslint/unbound-method */
 import { SongManager } from "../utils/SongManager";
 import { ISongs, ITextChannel } from "../../typings";
 import { Snowflake, VoiceChannel, VoiceConnection } from "discord.js";
@@ -7,14 +6,17 @@ export class ServerQueue {
     public connection: VoiceConnection | null = null;
     public readonly songs: ISongs = new SongManager();
     public volume = 0;
-    public playing = false;
     public loopMode: 0 | 1 | 2 = 0;
     public timeout: NodeJS.Timeout | null = null;
+    private _playing = false;
     private _lastMusicMessageID: Snowflake | null = null;
     private _lastvoiceStateUpdateMessageID: Snowflake | null = null;
     public constructor(public textChannel: ITextChannel | null = null, public voiceChannel: VoiceChannel | null = null) {
         this.volume = textChannel!.client.config.defaultVolume;
         Object.defineProperties(this, {
+            _playing: {
+                enumerable: false
+            },
             _lastMusicMessageID: {
                 enumerable: false
             },
@@ -22,6 +24,14 @@ export class ServerQueue {
                 enumerable: false
             }
         });
+    }
+
+    public get playing(): boolean {
+        return this._playing;
+    }
+
+    public set playing(state: boolean) {
+        this._playing = state;
     }
 
     public get lastMusicMessageID(): Snowflake | null {
