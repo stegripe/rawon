5119cc98d3972e581113235c7c4def4e54bff15f
zarrttt <undefinedgaming01@gmail.com>
2021-02-27 10:04:02 +0700

    chore: regenerate lockfile

--- src/utils/YouTube/index.ts
+++ src/utils/YouTube/index.ts
@@ -1,6 +1,6 @@
 import { YoutubeAPI } from "./utils/YouTubeAPI";
-import { YouTube as YouTubeSR, Video as VideoSR } from "youtube-sr";
-import { IMusicData, playMusic, IdownloadOptions, getMusicInfo, IMusicInfo } from "./downloader";
+import { YouTube as YouTubeSR } from "youtube-sr";
+import { IMusicData, playMusic, IdownloadOptions } from "./downloader";
 import { Video } from "./structures/Video";
 import { Playlist } from "./structures/Playlist";
 
@@ -17,7 +17,7 @@ export class YouTube {
         } else if (mode === "scrape") {
             this.engine = YouTubeSR;
         } else {
-            throw new Error("Unknown mode, available modes are 'api' and 'scrape'");
+            throw new Error("Unknown mode! Available modes are 'api' and 'scrape'.");
         }
     }
 
@@ -28,9 +28,9 @@ export class YouTube {
     public async getVideo(id: string): Promise<Video> {
         let data;
         if (this.mode === "api") data = await (this.engine as YoutubeAPI).getVideo(id);
-        if (this.mode === "scrape") data = (await getMusicInfo(`https://youtube.com/watch?v=${id}`)).videoDetails;
+        if (this.mode === "scrape") data = (await YouTubeSR.getVideo(`https://youtube.com/watch?v=${id}`));
         if (data === undefined) throw new Error("I could not get any data!");
-        return new Video(data, this.mode === "scrape" ? "ytdl" : "api");
+        return new Video(data, this.mode!);
     }
 
     public async getPlaylist(id: string): Promise<Playlist> {
@@ -38,18 +38,14 @@ export class YouTube {
         if (this.mode === "api") data = await (this.engine as YoutubeAPI).getPlaylist(id);
         if (this.mode === "scrape") data = (await YouTubeSR.getPlaylist(`https://youtube.com/playlist?list=${id}`));
         if (data === undefined) throw new Error("I could not get any data!");
-
         return new Playlist(data, this.mode!);
     }
 
     public async searchVideos(query: string, maxResults = 5): Promise<Video[]> {
         let data;
         if (this.mode === "api") data = await (this.engine as YoutubeAPI).searchVideos(query, maxResults);
-        if (this.mode === "scrape") data = (await YouTubeSR.search(query, { type: "video", limit: maxResults })) as unknown as VideoSR[];
+        if (this.mode === "scrape") data = (await YouTubeSR.search(query, { type: "video", limit: maxResults }));
         if (data === undefined) throw new Error("I could not get any data!");
-        // @ts-expect-error IGNORE
         return data.map((i: any) => new Video(i, this.mode!));
     }
 }
-
-export type YTDLInfo = IMusicInfo["videoDetails"];
