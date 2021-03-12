688c42f8cab970165ad01d89ec2659e7f14a0171
zarrttt <undefinedgaming01@gmail.com>
2021-03-12 17:57:47 +0700

    fix(InviteCommand): edit chaining and delete aliases

--- src/commands/InviteCommand.ts
+++ src/commands/InviteCommand.ts
@@ -5,7 +5,6 @@ import { createEmbed } from "../utils/createEmbed";
 import { disableInviteCmd } from "../config";
 
 @DefineCommand({
-    aliases: ["inv"],
     name: "invite",
     description: "Send the bot's invite link",
     usage: "{prefix}invite",
@@ -15,7 +14,7 @@ export class InviteCommand extends BaseCommand {
     public async execute(message: IMessage): Promise<void> {
         message.channel.send(
             createEmbed("info")
-                .addField(`${this.client.user?.tag} - Invite Link`, `**[Click here to invite this bot](${await this.client.generateInvite({ permissions: 53857345 })})**`)
+                .addField(`${this.client.user!.tag} - Invite Link`, `**[Click here to invite this bot](${await this.client.generateInvite({ permissions: 53857345 })})**`)
         ).catch(e => this.client.logger.error("PLAY_CMD_ERR:", e));
     }
 }
