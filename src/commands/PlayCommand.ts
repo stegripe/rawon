d9e4c0eb7632a82a4d487091eec947e446508d1f
PikaProgram <liroshea@yahoo.com>
2021-04-05 21:07:49 +0700

    fix(PlayCommand): reduce API usage when requesting spotify playlist (#236)
    Co-authored-by: PikaProgram <PikaProgram@github.com>

--- src/commands/PlayCommand.ts
+++ src/commands/PlayCommand.ts
@@ -4,6 +4,7 @@ import { ServerQueue } from "../structures/ServerQueue";
 import { Util, MessageEmbed, VoiceChannel } from "discord.js";
 // @ts-expect-error-next-line
 import { getData } from "spotify-url-info";
+import ytsr, { Video as SRVideo } from "ytsr";
 import { decodeHTML } from "entities";
 import { IMessage, ISong, IGuild, ITextChannel } from "../../typings";
 import { DefineCommand } from "../utils/decorators/DefineCommand";
@@ -82,8 +83,8 @@ export class PlayCommand extends BaseCommand {
             if (input[1] === "track") {
                 const trackData = await getData(url);
                 const trackSearchString = `${trackData.artists[0].name} - ${trackData.name}`;
-                const videoResult = await this.client.youtube.searchVideos(trackSearchString, 1);
-                const queuedVideo = await this.client.youtube.getVideo(videoResult[0].id);
+                const videoResult = await ytsr(trackSearchString, { limit: 1, safeSearch: false });
+                const queuedVideo = await this.client.youtube.getVideo((videoResult.items[0] as SRVideo).id);
                 return this.handleVideo(queuedVideo, message, voiceChannel);
             } else if (input[1] === "playlist") {
                 try {
@@ -95,8 +96,8 @@ export class PlayCommand extends BaseCommand {
                             .setThumbnail(playlistData.images[0].url)
                     );
                     for (const title of playlistSearchStrings) {
-                        const videoResults = await this.client.youtube.searchVideos(title, 1);
-                        const queuedVideo = await this.client.youtube.getVideo(videoResults[0].id);
+                        const videoResults = await ytsr(title, { limit: 1, safeSearch: false });
+                        const queuedVideo = await this.client.youtube.getVideo((videoResults.items[0] as SRVideo).id);
                         await this.handleVideo(queuedVideo, message, voiceChannel, true);
                     }
                     message.channel.messages.fetch(addingPlaylistVideoMessage.id, false).then(m => m.delete()).catch(e => this.client.logger.error("SP_PLAYLIST_ERR:", e));
