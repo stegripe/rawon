13f7abd771e8098f1aa6ee95d81eec65682433b7
Ahmad Yaqdhan <42838410+Mednoob@users.noreply.github.com>
2021-07-16 05:58:22 +0000

    feat(PlayCommand): add youtu.be link support (#360)
    * feat(PlayCommand): add youtu.be link support
    
    * fix(PlayCommand): change url variable name to fix variable name conflict
    
    * fix(PlayCommand): fix eslint issue
    
    * fix(PlayCommand): fix compiler issue

--- src/commands/PlayCommand.ts
+++ src/commands/PlayCommand.ts
@@ -97,7 +97,8 @@ export class PlayCommand extends BaseCommand {
             }
         }
         try {
-            const id = new URL(url).searchParams.get("v")!;
+            const fetchUrl = new URL(url);
+            const id = fetchUrl.hostname === "youtu.be" ? fetchUrl.pathname.replace(/\//g, "") : fetchUrl.searchParams.get("v")!;
             // eslint-disable-next-line no-var, block-scoped-var
             var video = await this.client.youtube.getVideo(id);
         } catch (e) {
