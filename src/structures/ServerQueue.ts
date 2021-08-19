d76b43a32b2addf4d33b8e653ce24253725e0681
mzrtamp <mozartrafikt@gmail.com>
2021-08-19 07:24:39 +0700

    refactor(ServerQueue): Remove unnecessary getter/setter

--- src/structures/ServerQueue.ts
+++ src/structures/ServerQueue.ts
@@ -1,5 +1,5 @@
-import { ISongs, ITextChannel } from "../../typings";
 import { SongManager } from "../utils/SongManager";
+import { ISongs, ITextChannel } from "../../typings";
 import { Snowflake, VoiceChannel, VoiceConnection } from "discord.js";
 
 export class ServerQueue {
@@ -8,48 +8,15 @@ export class ServerQueue {
     public volume = 0;
     public loopMode: 0 | 1 | 2 = 0;
     public timeout: NodeJS.Timeout | null = null;
-    private _playing = false;
-    private _lastMusicMessageID: Snowflake | null = null;
-    private _lastvoiceStateUpdateMessageID: Snowflake | null = null;
+    public playing = false;
+    public lastMusicMessageID: Snowflake | null = null;
+    public lastVoiceStateUpdateMessageID: Snowflake | null = null;
     public constructor(public textChannel: ITextChannel | null = null, public voiceChannel: VoiceChannel | null = null) {
         this.volume = textChannel!.client.config.defaultVolume;
         Object.defineProperties(this, {
-            _lastMusicMessageID: {
-                enumerable: false
-            },
-            _lastvoiceStateUpdateMessageID: {
-                enumerable: false
-            },
-            _playing: {
-                enumerable: false
-            },
             timeout: {
                 enumerable: false
             }
         });
-    }
-
-    public get playing(): boolean {
-        return this._playing;
-    }
-
-    public set playing(state: boolean) {
-        this._playing = state;
-    }
-
-    public get lastMusicMessageID(): Snowflake | null {
-        return this._lastMusicMessageID;
-    }
-
-    public set lastMusicMessageID(id: Snowflake | null) {
-        this._lastMusicMessageID = id;
-    }
-
-    public get lastVoiceStateUpdateMessageID(): Snowflake | null {
-        return this._lastvoiceStateUpdateMessageID;
-    }
-
-    public set lastVoiceStateUpdateMessageID(id: Snowflake | null) {
-        this._lastvoiceStateUpdateMessageID = id;
     }
 }
