e7943550c5a81e63344fd7a27332b35111034ae9
mzrtamp <mozartrafikt@gmail.com>
2021-08-19 08:40:13 +0700

    refactor(Inhibit): Use Message instead of IMessage

--- src/utils/decorators/Inhibit.ts
+++ src/utils/decorators/Inhibit.ts
@@ -1,10 +1,11 @@
-import { ICommandComponent, IMessage } from "../../typings";
+import { ICommandComponent } from "../../typings";
+import { Message } from "discord.js";
 
 export function Inhibit(func: ICommandComponent["execute"]) {
     return function decorate(target: unknown, key: string | symbol, descriptor: PropertyDescriptor): any {
         const original = descriptor.value;
         // eslint-disable-next-line func-names
-        descriptor.value = async function (message: IMessage, args: string[]): Promise<any> {
+        descriptor.value = async function (message: Message, args: string[]): Promise<any> {
             const result = await func(message, args);
             if (result === undefined) return original.apply(this, [message, args]);
             return null;
