af0a9945bb9ac197b5916ecf38092fe5264bf0d1
mzrtamp <mozartrafikt@gmail.com>
2021-08-20 13:28:57 +0700

    feat(ReadyEvent): Migrate to Discord.js v13

--- src/events/ReadyEvent.ts
+++ src/events/ReadyEvent.ts
@@ -6,8 +6,8 @@ export class ReadyEvent extends BaseListener {
     public execute(): void {
         this.client.logger.info(
             `${this.client.shard ? `[Shard #${this.client.shard.ids[0]}]` : ""} I'm ready to serve ${this.client.guilds.cache.size} guilds ` +
-            `with ${this.client.channels.cache.filter(c => c.type === "text").size} text channels and ` +
-            `${this.client.channels.cache.filter(c => c.type === "voice").size} voice channels`
+            `with ${this.client.channels.cache.filter(c => c.type === "GUILD_TEXT").size} text channels and ` +
+            `${this.client.channels.cache.filter(c => c.type === "GUILD_VOICE").size} voice channels`
         );
         this.doPresence();
     }
