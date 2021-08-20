90f38e1af094d2123832a21551e952460ab65d8e
mzrtamp <mozartrafikt@gmail.com>
2021-08-20 13:17:56 +0700

    feat(main): Migrate to Discord.js v13

--- src/main.ts
+++ src/main.ts
@@ -32,4 +32,4 @@ manager.on("shardCreate", shard => {
         log.info(`[ShardManager] Shard #${shard.id} has reconnected`);
     });
     if (manager.shards.size === manager.totalShards) log.info("[ShardManager] All shards has spawned successfully.");
-}).spawn(totalShards).catch(e => log.error("SHARD_SPAWN_ERR: ", e));
+}).spawn().catch(e => log.error("SHARD_SPAWN_ERR: ", e));
