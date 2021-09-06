8797101e6f0ab5da5b4de0e345e032d1ec3196ba
mzrtamp <mozartrafikt@gmail.com>
2021-09-06 10:16:03 +0700

    refactor(*): Prune them all
    This prevents conflicts

--- src/events/WarnEvent.ts
+++
@@ -1,9 +0,0 @@
-import { DefineEvent } from "../utils/decorators/DefineEvent";
-import { BaseEvent } from "../structures/BaseEvent";
-
-@DefineEvent("warn")
-export class WarnEvent extends BaseEvent {
-    public execute(warn: string): void {
-        this.client.logger.warn("CLIENT_WARN:", warn);
-    }
-}
--- src/utils/youtube/utils/YouTubeError.ts
+++
@@ -1,8 +0,0 @@
-export class YouTubeError extends Error {
-    public constructor(message: string, originalErr?: Error) {
-        super(message);
-        this.name = "YouTubeError";
-        this.stack = `${message}${originalErr ? `, err: ${originalErr.stack!}` : ""}`;
-        this.message = `${message}${originalErr ? `, err: ${originalErr.message}` : ""}`;
-    }
-}
