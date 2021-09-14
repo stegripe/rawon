e5f6cfc8fd6f6b8dd6f397727aca64d92be4e98c
mzrtamp <mozartrafikt@gmail.com>
2021-09-14 16:32:29 +0700

    chore: prune 'em all

--- src/events/DebugEvent.ts
+++
@@ -1,9 +0,0 @@
-import { BaseEvent } from "../structures/BaseEvent";
-import { DefineEvent } from "../utils/decorators/DefineEvent";
-
-@DefineEvent("debug")
-export class DebugEvent extends BaseEvent {
-    public execute(message: string): void {
-        this.client.logger.debug(message);
-    }
-}
