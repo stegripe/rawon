37a5b515277ec4df5690e857267ac8ed4478fb86
Mednoob <ahmadyaqdhan123@gmail.com>
2021-09-17 00:16:51 +0900

    feat(SongManager): add key value to queue song data

--- src/utils/SongManager.ts
+++ src/utils/SongManager.ts
@@ -4,11 +4,12 @@ import { Collection, Snowflake, SnowflakeUtil } from "discord.js";
 export class SongManager extends Collection<Snowflake, IQueueSong> {
     public addSong(song: ISong): Snowflake {
         const firstIndex = this.sort((a, b) => b.index - a.index).first()?.index;
+        const key = SnowflakeUtil.generate();
         const data: IQueueSong = {
             index: firstIndex ? firstIndex + 1 : 0,
+            key,
             song
         };
-        const key = SnowflakeUtil.generate();
 
         this.set(key, data);
         return key;
