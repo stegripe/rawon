249442acb7ce8bbed06e8d787793fac7b2619bd0
Mednoob <ahmadyaqdhan123@gmail.com>
2021-09-16 23:15:33 +0900

    feat(ServerQueue): add songs property

--- src/structures/ServerQueue.ts
+++ src/structures/ServerQueue.ts
@@ -1,9 +1,11 @@
+import { SongManager } from "../utils/SongManager";
 import { AudioPlayer, AudioPlayerStatus, VoiceConnection } from "@discordjs/voice";
 
 export class ServerQueue {
     public loopMode: 0 | 1 | 2 = 0;
     public connection: VoiceConnection|null = null;
     public player: AudioPlayer|null = null;
+    public readonly songs = new SongManager();
 
     public set playing(value: boolean) {
         if (value) {
