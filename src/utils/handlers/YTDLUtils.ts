615e2c1a1fe93a6f7ebe61a3c9af96d6072b777c
Mednoob <ahmadyaqdhan123@gmail.com>
2021-09-16 17:13:32 +0900

    feat(YTDLUtils): add getInfo function

--- src/utils/handlers/YTDLUtils.ts
+++ src/utils/handlers/YTDLUtils.ts
@@ -1,4 +1,4 @@
-import { raw } from "youtube-dl-exec";
+import ytdl, { raw, YtResponse } from "youtube-dl-exec";
 import { Readable } from "stream";
 
 export function getStream(url: string): Promise<Readable> {
@@ -24,4 +24,8 @@ export function getStream(url: string): Promise<Readable> {
             resolve(stream.stdout!);
         });
     });
+}
+
+export async function getInfo(url: string): Promise<YtResponse> {
+    return ytdl(url);
 }
