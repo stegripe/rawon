4e3987f8e86e4d876e15173e86634ca1c1e0a12a
Mednoob <ahmadyaqdhan123@gmail.com>
2021-09-16 12:55:05 +0900

    feat(GeneralUtil): check whether it's playlist or track or unknown in checkQuery function

--- src/utils/handlers/GeneralUtil.ts
+++ src/utils/handlers/GeneralUtil.ts
@@ -23,9 +23,43 @@ export function checkQuery(string: string): QueryData {
             sourceType: "query"
         };
     }
-    return {
-        isURL: true,
-        // Returns "soundcloud" | "youtube" | "spotify"
-        sourceType: /soundcloud/g.exec(url.hostname) ? "soundcloud" : /spotify/g.exec(url.hostname) ? "spotify" : /youtube/g.exec(url.hostname) ? "youtube" : undefined
+
+    const result: QueryData = {
+        isURL: true
     };
+
+    if (/soundcloud|snd/g.exec(url.hostname)) {
+        result.sourceType = "soundcloud";
+
+        if (url.pathname.includes("/sets/")) {
+            result.type = "playlist";
+        } else {
+            result.type = "track";
+        }
+    } else if (/youtube|youtu\.be/g.exec(url.hostname)) {
+        result.sourceType = "youtube";
+
+        if (!/youtu\.be/g.exec(url.hostname) && url.pathname.startsWith("/playlist")) {
+            result.type = "playlist";
+        } else if ((/youtube/g.exec(url.hostname) && url.pathname.startsWith("/watch")) || (/youtu\.be/g.exec(url.hostname) && (url.pathname !== ""))) {
+            result.type = "track";
+        } else {
+            result.type = "unknown";
+        }
+    } else if (/spotify/g.exec(url.hostname)) {
+        result.sourceType = "spotify";
+
+        if (url.pathname.startsWith("/playlist")) {
+            result.type = "playlist";
+        } else if (url.pathname.startsWith("/track")) {
+            result.type = "track";
+        } else {
+            result.type = "unknown";
+        }
+    } else {
+        result.sourceType = "unknown";
+        result.type = "unknown";
+    }
+
+    return result;
 }
