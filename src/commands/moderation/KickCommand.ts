9689ad2ec22ed87fd62e31f15bf76e2d2960c25a
mzrtamp <mozartrafikt@gmail.com>
2021-09-18 13:02:43 +0700

    feat(KickCommand): update string content

--- src/commands/moderation/KickCommand.ts
+++ src/commands/moderation/KickCommand.ts
@@ -6,7 +6,7 @@ import { User } from "discord.js";
 
 @DefineCommand({
     contextUser: "Kick Member",
-    description: "Kick member",
+    description: "Kick someone from the server",
     name: "kick",
     slash: {
         name: "kick",
@@ -23,16 +23,16 @@ import { User } from "discord.js";
 })
 export class KickCommand extends BaseCommand {
     public async execute(ctx: CommandContext): Promise<any> {
-        if (!ctx.member?.permissions.has("KICK_MEMBERS")) return ctx.reply({ embeds: [createEmbed("error", "You don't have permission to use this command")] });
-        if (!ctx.guild?.me?.permissions.has("KICK_MEMBERS")) return ctx.reply({ embeds: [createEmbed("error", "I don't have `Kick Members` permission")] });
+        if (!ctx.member?.permissions.has("KICK_MEMBERS")) return ctx.reply({ embeds: [createEmbed("error", "Sorry, but you don't have **`KICK MEMBERS`** permission to use this command.")] });
+        if (!ctx.guild?.me?.permissions.has("KICK_MEMBERS")) return ctx.reply({ embeds: [createEmbed("error", "Sorry, but I don't have **`KICK MEMBERS`** permission.")] });
 
         const memberId = ctx.isContextMenu() ? (ctx.additionalArgs.get("options") as User).id : (ctx.isInteraction() ? ctx.options?.getUser("Member", true).id : ctx.args[0]?.replace(/[^0-9]/g, ""));
         const member = ctx.guild.members.resolve(memberId!);
 
-        if (!member) return ctx.reply({ embeds: [createEmbed("error", "Please, mention a member")] });
-        if (!member.kickable) return ctx.reply({ embeds: [createEmbed("error", "I can't kick that member")] });
+        if (!member) return ctx.reply({ embeds: [createEmbed("error", "Please specify someone.")] });
+        if (!member.kickable) return ctx.reply({ embeds: [createEmbed("error", "I can't kick that member.")] });
 
         await member.kick();
-        return ctx.reply({ embeds: [createEmbed("success", `\`${member.user.tag}\` has been kicked from the server`)] });
+        return ctx.reply({ embeds: [createEmbed("success", `**${member.user.tag}** has been **\`KICKED\`** from the server.`)] });
     }
 }
