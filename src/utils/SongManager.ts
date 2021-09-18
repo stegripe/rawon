e5201d72a399604ca25328fe4235552e2dbb50af
Mednoob <ahmadyaqdhan123@gmail.com>
2021-09-18 21:42:08 +0900

    refactor(SongManager): use sortByIndex instead

--- src/utils/SongManager.ts
+++ src/utils/SongManager.ts
@@ -3,7 +3,7 @@ import { Collection, Snowflake, SnowflakeUtil } from "discord.js";
 
 export class SongManager extends Collection<Snowflake, IQueueSong> {
     public addSong(song: ISong): Snowflake {
-        const firstIndex = this.sort((a, b) => b.index - a.index).first()?.index;
+        const firstIndex = this.sortByIndex().first()?.index;
         const key = SnowflakeUtil.generate();
         const data: IQueueSong = {
             index: firstIndex ? firstIndex + 1 : 0,
