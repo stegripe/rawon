970ab5bc5c8ef76abc26aaa10e33f13b31d78932
Mednoob <ahmadyaqdhan123@gmail.com>
2021-09-20 09:38:33 +0900

    feat(QueueCommand): highlight now-playing song in queue list

--- src/commands/music/QueueCommand.ts
+++ src/commands/music/QueueCommand.ts
@@ -2,9 +2,11 @@ import { DefineCommand } from "../../utils/decorators/DefineCommand";
 import { haveQueue } from "../../utils/decorators/MusicUtil";
 import { CommandContext } from "../../structures/CommandContext";
 import { BaseCommand } from "../../structures/BaseCommand";
+import { IQueueSong } from "../../typings";
 import { ButtonPagination } from "../../utils/ButtonPagination";
 import { chunk } from "../../utils/chunk";
 import { createEmbed } from "../../utils/createEmbed";
+import { AudioPlayerPlayingState } from "@discordjs/voice";
 
 @DefineCommand({
     aliases: ["q"],
@@ -20,7 +22,12 @@ export class QueueCommand extends BaseCommand {
     public async execute(ctx: CommandContext): Promise<any> {
         const songs = ctx.guild!.queue!.songs.sortByIndex();
         const pages = await Promise.all(chunk([...songs.values()], 10).map(async (s, n) => {
-            const names = await Promise.all(s.map((song, i) => `${(n * 10) + (i + 1)} - [${song.song.title}](${song.song.url})`));
+            const names = await Promise.all(s.map((song, i) => {
+                const npKey = ((ctx.guild!.queue!.player!.state as AudioPlayerPlayingState).resource.metadata as IQueueSong).key;
+                const addition = song.key === npKey ? "**" : "";
+
+                return `${addition}${(n * 10) + (i + 1)} - [${song.song.title}](${song.song.url})${addition}`;
+            }));
 
             return names.join("\n");
         }));
