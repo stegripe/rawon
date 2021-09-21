41a75d524411913924541922b6cc17a34d44131e
mzrtamp <mozartrafikt@gmail.com>
2021-09-21 11:26:15 +0700

    chore(WarnCommand): don't return if the user disabled their DM
    And also updated string content

--- src/commands/moderation/WarnCommand.ts
+++ src/commands/moderation/WarnCommand.ts
@@ -10,7 +10,7 @@ import { createEmbed } from "../../utils/createEmbed";
         name: "warn",
         options: [
             {
-                description: "Member that will be warned",
+                description: "Who do you like to warn?",
                 name: "Member",
                 required: true,
                 type: "USER"
@@ -23,7 +23,7 @@ import { createEmbed } from "../../utils/createEmbed";
             }
         ]
     },
-    usage: "{prefix}warn <member> [reason]"
+    usage: "{prefix}warn <@mention | id> [reason]"
 })
 export class WarnCommand extends BaseCommand {
     public async execute(ctx: CommandContext): Promise<any> {
@@ -33,16 +33,16 @@ export class WarnCommand extends BaseCommand {
         if (!member) return ctx.reply({ embeds: [createEmbed("warn", "Please specify someone.")] });
 
         const dm = await ctx.member.user.createDM().catch(() => undefined);
-        if (!dm) return ctx.reply({ embeds: [createEmbed("error", "Unable to create a DM with that user")] });
+        if (!dm) await ctx.reply({ embeds: [createEmbed("error", "Unable to create a DM with that user, but I'll keep warn 'em.")] });
 
-        const reason = (ctx.isInteraction() ? ctx.options?.getString("Reason") : (ctx.args.length >= 2 ? ctx.args.slice(1, ctx.args.length).join(" ") : undefined)) ?? "[Reason not specified]";
+        const reason = (ctx.isInteraction() ? ctx.options?.getString("Reason") : (ctx.args.length >= 2 ? ctx.args.slice(1, ctx.args.length).join(" ") : undefined)) ?? "[Not Specified]";
         const embed = createEmbed("warn")
             .setAuthor(`Moderator: ${ctx.author.tag}`, ctx.author.displayAvatarURL({ dynamic: true }))
             .setDescription(`You have been warned on ${ctx.guild!.name}`)
-            .addField("Reason", `\`\`\`${reason}\`\`\``)
+            .addField("Reason", `\`\`\`\n${reason}\`\`\``)
             .setTimestamp(Date.now());
 
-        await dm.send({ embeds: [embed] });
+        await dm?.send({ embeds: [embed] });
         return ctx.reply({ embeds: [createEmbed("success", `${member.tag} has been warned`)] });
     }
 }
