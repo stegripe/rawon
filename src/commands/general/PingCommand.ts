03c5ef8db8e7183ec7ba6480fef4d7b357ed3ff4
Mednoob <ahmadyaqdhan123@gmail.com>
2021-09-23 22:45:31 +0900

    feat(PingCommand): add voice ping information

--- src/commands/general/PingCommand.ts
+++ src/commands/general/PingCommand.ts
@@ -21,6 +21,7 @@ export class PingCommand extends BaseCommand {
         const msg = await ctx.send({ content: "🏓" });
         const latency = Date.now() - before;
         const wsLatency = this.client.ws.ping.toFixed(0);
+        const vcLatency = ctx.guild?.queue?.connection?.ping.ws?.toFixed(0) ?? "N/A";
         const embed = createEmbed("info")
             .setColor(this.searchHex(wsLatency))
             .setAuthor("🏓 PONG", this.client.user!.displayAvatarURL())
@@ -31,6 +32,10 @@ export class PingCommand extends BaseCommand {
             }, {
                 name: "🌐 **|** WebSocket",
                 value: `**\`${wsLatency}\`** ms`,
+                inline: true
+            }, {
+                name: "🔊 **|** Voice",
+                value: `**\`${vcLatency}\`** ms`,
                 inline: true
             })
             .setFooter(`Latency of: ${this.client.user!.tag}`, this.client.user!.displayAvatarURL())
