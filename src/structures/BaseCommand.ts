fa5739f02ff6840b2e06680de13ba41fda6bcadd
Mednoob <ahmadyaqdhan123@gmail.com>
2021-09-24 18:25:39 +0900

    feat(BaseCommand): reload function is not used

--- src/structures/BaseCommand.ts
+++ src/structures/BaseCommand.ts
@@ -9,15 +9,4 @@ export class BaseCommand implements ICommandComponent {
 
     // eslint-disable-next-line @typescript-eslint/no-empty-function
     public execute(message: CommandContext, ...any: any): any {}
-
-    public reload(): ICommandComponent {
-        // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
-        delete require.cache[require.resolve(this.meta.path!)];
-        // eslint-disable-next-line @typescript-eslint/no-var-requires
-        const newCMD = new (require(this.meta.path!).default)(this.client, { path: this.meta.path, category: this.meta.category });
-        this.client.commands.get(this.meta.name)!.execute = newCMD.execute;
-        this.client.commands.get(this.meta.name)!.meta = newCMD.meta;
-        this.client.logger.info(`Command ${this.meta.name} from ${this.meta.category!} category has been reloaded.`);
-        return this.client.commands.get(this.meta.name)!;
-    }
 }
