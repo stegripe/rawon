d918cda7e1e5fbfcf6ad3e568003e8eefd676bf0
Alexander <47733425+lxndr-rl@users.noreply.github.com>
2021-09-24 13:55:27 -0500

    Revert "Merge branch 'feat/rewrite' into feat/rewrite"
    This reverts commit c2fe14fedf443f15b5f035986152f4d7b775586e, reversing
    changes made to 04a26a7705266dd4494b7e2578988b1f2ac6a719.

--- src/commands/music/LyricsCommand.ts
+++ src/commands/music/LyricsCommand.ts
@@ -11,8 +11,6 @@ import { AudioPlayerPlayingState, AudioResource } from "@discordjs/voice";
     description: "Show the lyrics from current/requested song",
     name: "lyrics",
     slash: {
-        description: "Show the lyrics from current/requested song",
-        name: "lyrics",
         options: [
             {
                 description: "Song to search",
@@ -30,17 +28,6 @@ export class LyricsCommand extends BaseCommand {
         if (!query) return ctx.reply({ embeds: [createEmbed("error", "There is nothing playing or no args inserted")] });
 
         return this.getLyrics(ctx, query);
-        if (ctx.args.length >= 1) {
-            void this.getLyrics(ctx, ctx.args.join(" "));
-        } else {
-            let song;
-            try {
-                song = ((ctx.guild!.queue!.player!.state as AudioPlayerPlayingState).resource.metadata as IQueueSong).song.title;
-            } catch {
-                return ctx.reply({ embeds: [createEmbed("error", "There is nothing playing or no args inserted")] });
-            }
-            void this.getLyrics(ctx, song);
-        }
     }
 
     private async getLyrics(ctx: CommandContext, song: string): Promise<any> {
@@ -82,46 +69,6 @@ export class LyricsCommand extends BaseCommand {
                     pages
                 })).start();
             })
-        void await fetch(url).then(
-            response => response.json()
-        ).then(async (data: any) => {
-            if (data.error) {
-                return ctx.reply({ embeds: [createEmbed("error", `The API could not find the song ${song}\n${data.message}`)] });
-            }
-            let lyrics: string = data.lyrics;
-            let albumArt: string = data.album_art;
-            const charLength: number = lyrics.length;
-            let cantEmbeds = 0;
-
-            if (charLength < 2048) {
-                cantEmbeds = 1;
-            } else {
-                for (let i = 2; i < 10; i++) {
-                    if (charLength < 2048 * i) {
-                        cantEmbeds = i;
-                        break;
-                    }
-                }
-            }
-            const lyricsArr: any = [];
-            if (!albumArt) albumArt = "https://api.zhycorp.com/assets/images/logo.png";
-            lyricsArr.push([lyrics.substring(0, 2047)]);
-            lyrics = lyrics.replace(lyrics.substring(0, 2047), "");
-            for (let i = 2; i <= cantEmbeds; i++) {
-                lyricsArr.push([lyrics.substring(0, 2047)]);
-                lyrics = lyrics.replace(lyrics.substring(0, 2048), "");
-            }
-            const pages: any = await Promise.all(lyricsArr.map(async (s: any) => s.join("\n")));
-
-            const embed = createEmbed("info", pages[0]).setAuthor(`${data.song} - ${data.artist}`).setThumbnail(albumArt);
-            const msg = await ctx.reply({ embeds: [embed] });
-            return (new ButtonPagination(msg, {
-                author: ctx.author.id,
-                edit: (i, e, p) => e.setDescription(p).setFooter(`Page ${i + 1} of ${pages.length}`),
-                embed,
-                pages
-            })).start();
-        })
             .catch(error => console.error(error));
     }
 }
