642c1095c3d3db138fd8f54bb4327de3f1ac8e77
Mednoob <ahmadyaqdhan123@gmail.com>
2021-09-25 11:20:09 +0900

    feat(ClientUtils): add 'fetchMuteRole' function

--- src/utils/ClientUtils.ts
+++ src/utils/ClientUtils.ts
@@ -1,7 +1,17 @@
 import { Disc } from "../structures/Disc";
+import { Guild, Role } from "discord.js";
 
 export class ClientUtils {
     public constructor(public readonly client: Disc) {}
+
+    public async fetchMuteRole(guild: Guild): Promise<Role> {
+        return guild.roles.cache.find(x => x.name === this.client.config.muteRoleName) ?? guild.roles.create({
+            mentionable: false,
+            name: this.client.config.muteRoleName,
+            permissions: ["VIEW_CHANNEL", "READ_MESSAGE_HISTORY"],
+            reason: "Create mute role"
+        });
+    }
 
     public decode(string: string): string {
         return Buffer.from(string, "base64").toString("ascii");
