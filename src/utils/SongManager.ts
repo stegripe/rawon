08d628cbc93c27e4d008c19e1e8d2543b7bc4f2d
Mednoob <ahmadyaqdhan123@gmail.com>
2021-09-25 15:06:14 +0900

    feat(SongManager): add requester args in addSong function

--- src/utils/SongManager.ts
+++ src/utils/SongManager.ts
@@ -1,13 +1,14 @@
 import { ISong, IQueueSong } from "../typings";
-import { Collection, Snowflake, SnowflakeUtil } from "discord.js";
+import { Collection, GuildMember, Snowflake, SnowflakeUtil } from "discord.js";
 
 export class SongManager extends Collection<Snowflake, IQueueSong> {
-    public addSong(song: ISong): Snowflake {
+    public addSong(song: ISong, requester: GuildMember): Snowflake {
         const firstIndex = this.sortByIndex().first()?.index;
         const key = SnowflakeUtil.generate();
         const data: IQueueSong = {
             index: firstIndex ? firstIndex + 1 : 0,
             key,
+            requester,
             song
         };
 
