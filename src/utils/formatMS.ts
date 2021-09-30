890305fe1e1cc53d03a4db21fd3d01f8c2500b1e
Mednoob <ahmadyaqdhan123@gmail.com>
2021-09-30 14:32:13 +0900

    feat(formatMS): add localization support

--- src/utils/formatMS.ts
+++ src/utils/formatMS.ts
@@ -1,6 +1,16 @@
-import { formatDuration, intervalToDuration } from "date-fns";
+import { lang } from "../config";
+import { formatDuration, intervalToDuration, Locale } from "date-fns";
+import { existsSync } from "fs";
+import { resolve } from "path";
 
 export function formatMS(ms: number): string {
     if (isNaN(ms)) throw new Error("value is not a number.");
-    return formatDuration(intervalToDuration({ start: 0, end: ms }));
+
+    const folderName = existsSync(resolve(process.cwd(), "node_modules", "date-fns", "locale", lang)) ? lang : "en-US";
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const locale = require(`date-fns/locale/${folderName}`) as Locale;
+
+    return formatDuration(intervalToDuration({ start: 0, end: ms }), {
+        locale
+    });
 }
