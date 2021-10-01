1899eb65707954964423a36beaaa0b0760bec99e
Mednoob <ahmadyaqdhan123@gmail.com>
2021-10-01 12:15:19 +0900

    feat(index): StrictMode is not needed

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,5 @@
 import App from './App';
 import './index.css';
-import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
