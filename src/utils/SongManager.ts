6002212395597bf431358f9e8ee77010ea24f24d
Mednoob <ahmadyaqdhan123@gmail.com>
2021-10-03 08:17:30 +0900

    fix(SongManager): change 'firstIndex' value in 'addSong'

--- src/utils/SongManager.ts
+++ src/utils/SongManager.ts
@@ -3,7 +3,7 @@ import { Collection, GuildMember, Snowflake, SnowflakeUtil } from "discord.js";
 
 export class SongManager extends Collection<Snowflake, IQueueSong> {
     public addSong(song: ISong, requester: GuildMember): Snowflake {
-        const firstIndex = this.sortByIndex().first()?.index;
+        const firstIndex = [...this.sortByIndex().values()].reverse()[0]?.index;
         const key = SnowflakeUtil.generate();
         const data: IQueueSong = {
             index: firstIndex ? firstIndex + 1 : 0,
