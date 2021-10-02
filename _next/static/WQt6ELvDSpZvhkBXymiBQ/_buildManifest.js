df9bf71f43faf6979ae056dcdc3183d54d0eae56
Mednoob <42838410+Mednoob@users.noreply.github.com>
2021-10-02 07:57:44 +0000

    Deploying to gh-pages from @ zhycorp/disc-11@bd0f8a89c30dd5377e548cd864a38ef4302f598c 🚀

---
+++ _next/static/WQt6ELvDSpZvhkBXymiBQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-fc5adc0e632320fd705d.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
