cad9ffee5681c5145fac0cdf87e6986bd62929b3
Mednoob <ahmadyaqdhan123@gmail.com>
2021-10-02 13:20:20 +0900

    refactor(index): move 'App' function to 'index.tsx'

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="flex items-center justify-center min-w-full h-screen dark:bg-gray-900">
-      <p className="m-32 text-6xl font-bold dark:text-white">Coming soon!</p>
-    </div>
-  );
-}
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
-import App from './App';
 import './index.css';
 import ReactDOM from 'react-dom';
+
+function App() {
+    return (
+        <div className="flex items-center justify-center min-w-full h-screen dark:bg-gray-900">
+            <p className="m-32 text-6xl font-bold dark:text-white">Coming soon!</p>
+        </div>
+    );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
