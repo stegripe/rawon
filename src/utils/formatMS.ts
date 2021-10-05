936efbfebae68a34bc885394c572a6f5094534fc
Mednoob <ahmadyaqdhan123@gmail.com>
2021-10-05 15:16:21 +0900

    refactor(formatMS): use 'date-fns/locale' module instead of manual importing for localization

--- src/utils/formatMS.ts
+++ src/utils/formatMS.ts
@@ -1,14 +1,12 @@
 import { lang } from "../config";
 import { formatDuration, intervalToDuration, Locale } from "date-fns";
-import { existsSync } from "fs";
-import { resolve } from "path";
+import Locales from "date-fns/locale";
 
 export function formatMS(ms: number): string {
     if (isNaN(ms)) throw new Error("value is not a number.");
 
-    const folderName = existsSync(resolve(process.cwd(), "node_modules", "date-fns", "locale", lang)) ? lang : "en-US";
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const locale = require(`date-fns/locale/${folderName}`) as Locale;
+    const locales = Locales as Record<string, Locale|undefined>;
+    const locale = locales[lang] ?? locales.enUS;
 
     return formatDuration(intervalToDuration({ start: 0, end: ms }), {
         locale
