c9f0f6713b8da458d9751a2c47b7d8397877e0bb
Mednoob <ahmadyaqdhan123@gmail.com>
2021-10-05 15:49:54 +0900

    fix(formatMS): remove localization support

--- src/utils/formatMS.ts
+++ src/utils/formatMS.ts
@@ -1,14 +1,7 @@
-import { lang } from "../config";
-import { formatDuration, intervalToDuration, Locale } from "date-fns";
-import Locales from "date-fns/locale";
+import { formatDuration, intervalToDuration } from "date-fns";
 
 export function formatMS(ms: number): string {
     if (isNaN(ms)) throw new Error("value is not a number.");
 
-    const locales = Locales as Record<string, Locale|undefined>;
-    const locale = locales[lang] ?? locales.enUS;
-
-    return formatDuration(intervalToDuration({ start: 0, end: ms }), {
-        locale
-    });
+    return formatDuration(intervalToDuration({ start: 0, end: ms }));
 }
