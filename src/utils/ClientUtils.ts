5cbd814fd766f30535c4a71f3ed6e17c929b7811
Mednoob <ahmadyaqdhan123@gmail.com>
2021-10-08 11:28:34 +0900

    feat(ClientUtils): add 'getChannelCount' function

--- src/utils/ClientUtils.ts
+++ src/utils/ClientUtils.ts
@@ -45,4 +45,30 @@ export class ClientUtils {
 
         return arr.filter((x, i) => arr.indexOf(x) === i).length;
     }
+
+    public async getChannelCount(textOnly = true): Promise<number> {
+        let arr: string[] = [];
+
+        if (this.client.shard) {
+            const shardChannels = await this.client.shard.broadcastEval((c, t) => c.channels.cache.filter(ch => {
+                if (t) return ch.type === "GUILD_TEXT" || ch.type === "GUILD_PUBLIC_THREAD" || ch.type === "GUILD_PRIVATE_THREAD";
+
+                return true;
+            }).map(ch => ch.id), {
+                context: textOnly
+            });
+
+            for (const channels of shardChannels) {
+                arr = arr.concat(channels);
+            }
+        } else {
+            arr = this.client.channels.cache.filter(ch => {
+                if (textOnly) return ch.type === "GUILD_TEXT" || ch.type === "GUILD_PUBLIC_THREAD" || ch.type === "GUILD_PRIVATE_THREAD";
+
+                return true;
+            }).map(ch => ch.id);
+        }
+
+        return arr.filter((x, i) => arr.indexOf(x) === i).length;
+    }
 }
