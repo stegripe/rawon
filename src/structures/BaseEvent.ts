810666692d848ea400caf41c7b33b038aef9b569
Mednoob <ahmadyaqdhan123@gmail.com>
2021-10-16 11:08:51 +0900

    feat(BaseEvent): turn class into an abstract class

--- src/structures/BaseEvent.ts
+++ src/structures/BaseEvent.ts
@@ -1,9 +1,8 @@
 import { IEvent } from "../typings";
 import { Disc } from "./Disc";
 
-export class BaseEvent implements IEvent {
+export abstract class BaseEvent implements IEvent {
     public constructor(public client: Disc, public readonly name: IEvent["name"]) {}
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-function
-    public execute(...args: any): any {}
+    public abstract execute(...args: any): any;
 }
