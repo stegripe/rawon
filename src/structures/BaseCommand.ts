1c014353c99f8c8d273ebdc3d8500cf3075cadb0
Mednoob <ahmadyaqdhan123@gmail.com>
2021-10-16 11:08:06 +0900

    feat(BaseCommand): turn class into an abstract class

--- src/structures/BaseCommand.ts
+++ src/structures/BaseCommand.ts
@@ -4,9 +4,8 @@ import { ICommandComponent } from "../typings";
 import { Disc } from "./Disc";
 import { CommandInteraction, Message, SelectMenuInteraction } from "discord.js";
 
-export class BaseCommand implements ICommandComponent {
+export abstract class BaseCommand implements ICommandComponent {
     public constructor(public client: Disc, public meta: ICommandComponent["meta"]) {}
 
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    public execute(ctx: CommandContext): any {}
+    public abstract execute(ctx: CommandContext): any;
 }
