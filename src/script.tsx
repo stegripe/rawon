f491dd2722dd06db1b0a87b41201c8a841276a01
Mednoob <ahmadyaqdhan123@gmail.com>
2021-10-20 19:37:55 +0900

    feat(script): add more features

--- src/script.tsx
+++ src/script.tsx
@@ -1,38 +1,54 @@
 import './index.css';
 import "codemirror/lib/codemirror.css";
 import CodeMirror from "codemirror";
-import { useEffect } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 require("codemirror/mode/javascript/javascript");
 
 function Script() {
-    useEffect(() => {
-        const value = `function trigger() {
-    var url = [
-        "Project link",
-        "Project link",
-        "Project link"
-    ];
-    for (var x = 0; x < url.length; x++) {
-        var uri = url[x];
-            try {
-                UrlFetchApp.fetch(uri);
-            } catch (err) {
-                Logger.log(err.message)
-            }
-                    
-    }
-}`
+    const [state, change] = useState<{ text: string; items: string[] }>({ text: "", items: [] });
 
+    useEffect(() => {
+        document.getElementById("code")!.innerHTML = "";
         CodeMirror(document.getElementById("code")!, {
             lineNumbers: true,
             mode: "javascript",
             readOnly: true,
-            value
+            value: `function trigger() {
+    var url = [
+${state.items.map(x => `        "${x}"`).join(",\n")}
+    ];
+    for (var x = 0; x < url.length; x++) {
+        var uri = url[x];
+        try {
+            UrlFetchApp.fetch(uri);
+        } catch (err) {
+            Logger.log(err.message)
+        }
+    }
+}`
         })
-    })
+    });
+
+    function onChange(data: ChangeEvent<HTMLInputElement>) {
+        change({ items: state.items, text: data.target.value });
+    }
+
+    function onSubmit() {
+        change({ items: state.items.concat([state.text]), text: "" });
+    }
 
     return (
         <div className="flex items-center justify-center min-w-full h-screen dark:bg-gray-900">
+            <div className="m-20">
+                <p className="text-xl font-bold dark:text-white">Script Generator</p>
+                <br />
+                <input id="url-textbox" onChange={onChange} value={state.text} placeholder="Put URL here"/>
+                <br />
+                <button className="p-2 mt-2 transition-colors border rounded border-black hover:bg-black hover:text-white dark:text-white dark:border-white dark:hover:bg-white dark:hover:text-black" onClick={onSubmit}>
+                    Add URL
+                </button>
+            </div>
+            <br />
             <div id="code"></div>
         </div>
     )
