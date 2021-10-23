5ffdb63d422c69d6b4e6093a926a4317cd924c53
Mednoob <ahmadyaqdhan123@gmail.com>
2021-10-23 23:10:00 +0900

    feat(navbar): add 'Home' navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,11 +19,14 @@ export default function Navbar() {
         <div className="max-w-7xl mx-auto px-8">
           <div className="flex items-center justify-between h-16">
             <div className=" flex items-center">
-              <a className="flex-shrink-0" href="/">
+              <a className="flex-shrink-0" href="/#/">
                 <img className="h-8 w-8" src="https://disc-11.zhycorp.net/logo512.png" alt=""/>
               </a>
               <Popover.Group as="div" className="hidden md:block">
                 <div className="ml-10 flex items-baseline space-x-4">
+                  <a href="/#/" className="text-gray-500 dark:text-white dark:hover:text-gray-400 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium">
+                    Home
+                  </a>
                   <Popover className="relative">
                     {({ open }) => (
                       <>
