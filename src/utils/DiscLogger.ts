a7b30a93bd7281ad54ee75b7ccab3c39a198deb3
Mednoob <ahmadyaqdhan123@gmail.com>
2021-11-04 21:38:35 +0900

    feat(DiscLogger): add more functionality

--- src/utils/DiscLogger.ts
+++ src/utils/DiscLogger.ts
@@ -1,5 +1,31 @@
 import { IDiscLoggerOptions } from "../typings";
+import { format } from "date-fns";
+
+enum Colors {
+    Reset = "\x1b[0m",
+    Green = "\x1b[32m",
+    Blue = "\x1b[34m",
+    Red = "\x1b[31m"
+}
 
 export class DiscLogger {
     public constructor(public readonly options: IDiscLoggerOptions) {}
+
+    public info(...messages: any[]): void {
+        this.log(messages, "info");
+    }
+
+    public debug(...messages: any[]): void {
+        this.log(messages, "debug");
+    }
+
+    public error(...messages: any[]): void {
+        this.log(messages, "error");
+    }
+
+    private log(messages: any[], level: "info"|"debug"|"error" = "info"): void {
+        if (this.options.prod && level === "debug") return;
+
+        console.log(`${level === "debug" ? Colors.Blue : (level === "error" ? Colors.Red : Colors.Green)} [${format(Date.now(), "yyyy-MM-dd HH:mm:ss (x)")}] [${level}]: ${messages.map(x => String(x)).join(" ")} ${Colors.Reset}`);
+    }
 }
