83a7ac2562bd3371888bcd1094885ef85445bdb2
Mednoob <ahmadyaqdhan123@gmail.com>
2021-11-08 09:07:28 +0900

    feat(index): change message

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ function Index() {
         <div className="flex items-center justify-center min-w-full h-screen dark:bg-gray-900">
             <div className="grid grid-cols-1 grid-rows-2 gap-3 text-center">
                 <DiscSVG className="flex justify-self-center h-32 md:h-48 w-auto rounded-full" />
-                <p className="text-sm font-bold dark:text-white m-5">Welcome to Disc 11 website. We haven't added anything for this page. For now, you can use the tools made by us to help you develop your own Disc 11 discord bot.</p>
+                <p className="text-sm font-bold dark:text-white m-5">Welcome to Disc 11 website. We haven't added anything for this page. For now, you can use the tools made by us to help you develop your own Disc 11 Discord bot.</p>
             </div>
         </div>
     );
