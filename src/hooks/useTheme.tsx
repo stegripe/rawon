809d819f383524af9fe2decddbe449d381125711
Mednoob <ahmadyaqdhan123@gmail.com>
2021-11-28 12:36:44 +0900

    feat(useTheme): better theme-change handling

--- src/hooks/useTheme.tsx
+++ src/hooks/useTheme.tsx
@@ -5,16 +5,23 @@ type ThemeName = "dark" | "light";
 export function useTheme(): [ThemeName, (theme: ThemeName) => void] {
     const [theme, setTheme] = useState<ThemeName>(window.localStorage.getItem("disc11Theme") as ThemeName|null ?? "light");
 
+    window.addEventListener("storage", ev => {
+        if (ev.key === "disc11Theme" && (ev.oldValue !== ev.newValue)) {
+            if ((ev.oldValue ?? "light") === "light") {
+                setTheme("dark");
+            } else {
+                setTheme("light")
+            }
+        }
+    });
+
     return [theme, (newTheme: ThemeName) => {
         window.localStorage.setItem("disc11Theme", newTheme);
-        if (window.onstorage !== null) {
-            window.onstorage(new StorageEvent("storage", {
-                key: "disc11Theme",
-                newValue: newTheme,
-                oldValue: theme,
-                storageArea: window.localStorage
-            }))
-        }
-        setTheme(window.localStorage.getItem("disc11Theme") as ThemeName|null ?? "light");
+        window.dispatchEvent(new StorageEvent("storage", {
+            key: "disc11Theme",
+            newValue: newTheme,
+            oldValue: theme,
+            storageArea: window.localStorage
+        }));
     }]
 }
