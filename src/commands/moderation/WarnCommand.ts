fcd709dc00f4cfc45b300dd2ade42b9ab9a75830
Mednoob <ahmadyaqdhan123@gmail.com>
2022-02-07 21:49:16 +0900

    style(WarnCommand): better line styling

--- src/commands/moderation/WarnCommand.ts
+++ src/commands/moderation/WarnCommand.ts
@@ -30,16 +30,43 @@ export class WarnCommand extends BaseCommand {
     }
 
     public async execute(ctx: CommandContext): Promise<Message> {
-        if (!ctx.member?.permissions.has("MANAGE_GUILD")) return ctx.reply({ embeds: [createEmbed("error", i18n.__("commands.moderation.warn.userNoPermission"), true)] });
+        if (!ctx.member?.permissions.has("MANAGE_GUILD")) {
+            return ctx.reply({
+                embeds: [
+                    createEmbed("error", i18n.__("commands.moderation.warn.userNoPermission"), true)
+                ]
+            });
+        }
 
-        const member = ctx.guild?.members.resolve(ctx.args.shift()?.replace(/[^0-9]/g, "") ?? "")?.user ?? ctx.options?.getUser("member", true);
-        if (!member) return ctx.reply({ embeds: [createEmbed("warn", i18n.__("commands.moderation.common.noUserSpecified"))] });
+        const member = ctx.guild?.members.resolve(
+            ctx.args.shift()?.replace(/[^0-9]/g, "") ?? ""
+        )?.user ?? ctx.options?.getUser("member", true);
+        if (!member) {
+            return ctx.reply({
+                embeds: [
+                    createEmbed("warn", i18n.__("commands.moderation.common.noUserSpecified"))
+                ]
+            });
+        }
 
         const dm = await member.createDM().catch(() => undefined);
-        if (!dm) await ctx.reply({ embeds: [createEmbed("warn", i18n.__("commands.moderation.warn.noDM"))] });
+        if (!dm) {
+            await ctx.reply({
+                embeds: [
+                    createEmbed("warn", i18n.__("commands.moderation.warn.noDM"))
+                ]
+            });
+        }
 
-        const reason = ctx.options?.getString("reason") ?? (ctx.args.join(" ") || i18n.__("commands.moderation.common.noReasonString"));
-        const embed = createEmbed("warn", i18n.__mf("commands.moderation.warn.userWarned", { guildName: ctx.guild!.name }))
+        const reason = ctx.options?.getString("reason") ?? (
+            ctx.args.join(" ") || i18n.__("commands.moderation.common.noReasonString")
+        );
+        const embed = createEmbed(
+            "warn",
+            i18n.__mf("commands.moderation.warn.userWarned", {
+                guildName: ctx.guild!.name
+            })
+        )
             .addField(i18n.__("commands.moderation.common.reasonString"), reason)
             .setFooter({
                 text: i18n.__("commands.moderation.warn.warnedByString"),
@@ -48,6 +75,14 @@ export class WarnCommand extends BaseCommand {
             .setTimestamp(Date.now());
 
         await dm?.send({ embeds: [embed] });
-        return ctx.reply({ embeds: [createEmbed("success", i18n.__mf("commands.moderation.warn.warnSuccess", { user: member.tag }), true)] });
+        return ctx.reply({
+            embeds: [
+                createEmbed(
+                    "success",
+                    i18n.__mf("commands.moderation.warn.warnSuccess", { user: member.tag }),
+                    true
+                )
+            ]
+        });
     }
 }
