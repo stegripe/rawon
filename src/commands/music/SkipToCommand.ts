78aa1ffab2aa265967f60d76161aff810e6fe0d2
Mednoob <ahmadyaqdhan123@gmail.com>
2022-02-07 22:27:06 +0900

    style(SkipToCommand): better line styling

--- src/commands/music/SkipToCommand.ts
+++ src/commands/music/SkipToCommand.ts
@@ -51,13 +51,41 @@ export class SkipToCommand extends BaseCommand {
         if (!sameVC(ctx)) return;
 
         const djRole = await this.client.utils.fetchDJRole(ctx.guild!);
-        if (!ctx.member?.roles.cache.has(djRole.id) && !ctx.member?.permissions.has("MANAGE_GUILD")) return ctx.reply({ embeds: [createEmbed("error", i18n.__("commands.music.skipTo.noPermission"), true)] });
+        if (!ctx.member?.roles.cache.has(djRole.id) && !ctx.member?.permissions.has("MANAGE_GUILD")) {
+            return ctx.reply({
+                embeds: [
+                    createEmbed("error", i18n.__("commands.music.skipTo.noPermission"), true)
+                ]
+            });
+        }
 
-        const targetType = (ctx.args[0] as string | undefined) ?? ctx.options?.getSubcommand() ?? ctx.options?.getNumber("position");
-        if (!targetType) return ctx.reply({ embeds: [createEmbed("warn", i18n.__mf("reusable.invalidUsage", { prefix: `${this.client.config.mainPrefix}help`, name: `${this.meta.name}` }))] });
+        const targetType = (ctx.args[0] as string | undefined) ??
+            ctx.options?.getSubcommand() ??
+            ctx.options?.getNumber("position");
+        if (!targetType) {
+            return ctx.reply({
+                embeds: [
+                    createEmbed(
+                        "warn",
+                        i18n.__mf("reusable.invalidUsage", {
+                            prefix: `${this.client.config.mainPrefix}help`,
+                            name: `${this.meta.name}`
+                        })
+                    )
+                ]
+            });
+        }
 
         const songs = [...ctx.guild!.queue!.songs.sortByIndex().values()];
-        if (!["first", "last"].includes(String(targetType).toLowerCase()) && (!isNaN(Number(targetType)) && !songs[Number(targetType) - 1])) return ctx.reply({ embeds: [createEmbed("error", i18n.__("commands.music.skipTo.noSongPosition"), true)] });
+        if (!["first", "last"].includes(String(targetType).toLowerCase()) && (
+            !isNaN(Number(targetType)) && !songs[Number(targetType) - 1]
+        )) {
+            return ctx.reply({
+                embeds: [
+                    createEmbed("error", i18n.__("commands.music.skipTo.noSongPosition"), true)
+                ]
+            });
+        }
 
         let song: IQueueSong;
         if (String(targetType).toLowerCase() === "first") {
@@ -68,10 +96,25 @@ export class SkipToCommand extends BaseCommand {
             song = songs[Number(targetType) - 1];
         }
 
-        if (song.key === ((ctx.guild!.queue!.player!.state as AudioPlayerPlayingState).resource.metadata as IQueueSong).key) return ctx.reply({ embeds: [createEmbed("error", i18n.__("commands.music.skipTo.cantPlay"), true)] });
+        if (song.key === ((ctx.guild!.queue!.player!.state as AudioPlayerPlayingState).resource.metadata as IQueueSong).key) {
+            return ctx.reply({
+                embeds: [
+                    createEmbed("error", i18n.__("commands.music.skipTo.cantPlay"), true)
+                ]
+            });
+        }
 
         void play(this.client, ctx.guild!, song.key);
 
-        return ctx.reply({ embeds: [createEmbed("success", `⏭ **|** ${i18n.__mf("commands.music.skipTo.skipMessage", { song: `[${song.song.title}](${song.song.url})` })}`).setThumbnail(song.song.thumbnail)] });
+        return ctx.reply({
+            embeds: [
+                createEmbed(
+                    "success",
+                    `⏭ **|** ${i18n.__mf("commands.music.skipTo.skipMessage", {
+                        song: `[${song.song.title}](${song.song.url})`
+                    })}`
+                ).setThumbnail(song.song.thumbnail)
+            ]
+        });
     }
 }
