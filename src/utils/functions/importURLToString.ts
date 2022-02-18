01d2755ca197bb13453758af29b37bd7a4d618fd
Rasyad Satyatma <lirosphere@yahoo.com>
2022-02-18 08:53:13 +0700

    fix(*): Fix file url parsing for linux paths

--- src/utils/functions/importURLToString.ts
+++ src/utils/functions/importURLToString.ts
@@ -1,8 +1,16 @@
 import { URL } from "url";
+import { resolve, isAbsolute, normalize, relative } from "path";
+import { platform } from "os";
 
 export function importURLToString(url: string): string {
     const paths = new URL(url).pathname.split(/\/|\\/g).filter(Boolean);
 
     paths.pop();
+
+    if (!isAbsolute(paths.join("/"))) {
+        const hostPlatform = platform();
+        if (hostPlatform === "linux")
+            return decodeURIComponent(`/${paths.join("/")}`);
+    }
     return decodeURIComponent(paths.join("/"));
 }
