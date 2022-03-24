239421d305786f3b6bf9d202ca3931577594a17c
Mednoob <ahmadyaqdhan123@gmail.com>
2022-03-24 18:35:04 +0900

    style(index): use one-line declaration for functions

--- src/index.ts
+++ src/index.ts
@@ -21,20 +21,13 @@ if (enableRepl) {
     });
 
     repl.context.shardManager = manager;
-    process.stdin.on("data", _ => {
-        repl.displayPrompt(true);
-    });
-    repl.on("exit", () => {
-        process.exit();
-    });
+    process.stdin.on("data", _ => repl.displayPrompt(true));
+    repl.on("exit", () => process.exit());
 }
 
 manager.on("shardCreate", shard => {
     log.info(`[ShardManager] Shard #${shard.id} Spawned.`);
-    shard.on("disconnect", () => {
-        log.warn("SHARD_DISCONNECTED: ", { stack: `[ShardManager] Shard #${shard.id} has disconnected.` });
-    }).on("reconnection", () => {
-        log.info(`[ShardManager] Shard #${shard.id} has reconnected.`);
-    });
+    shard.on("disconnect", () => log.warn("SHARD_DISCONNECTED: ", { stack: `[ShardManager] Shard #${shard.id} has disconnected.` }))
+        .on("reconnection", () => log.info(`[ShardManager] Shard #${shard.id} has reconnected.`));
     if (manager.shards.size === manager.totalShards) log.info("[ShardManager] All shards are spawned successfully.");
 }).spawn().catch(e => log.error("SHARD_SPAWN_ERR: ", e));
