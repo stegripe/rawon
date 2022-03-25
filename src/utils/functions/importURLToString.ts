b03faf8a026d69d808eda8e84819c8178d43fbfc
Mednoob <ahmadyaqdhan123@gmail.com>
2022-03-25 17:09:19 +0900

    fix(importURLToString): fix wrong slicing

--- src/utils/functions/importURLToString.ts
+++ src/utils/functions/importURLToString.ts
@@ -3,7 +3,7 @@ import { platform } from "os";
 
 export function importURLToString(url: string): string {
     const pathArray = new URL(url).pathname.split(/\/|\\/g).filter(Boolean);
-    const path = pathArray.slice(1).join("/");
+    const path = pathArray.slice(0, -1).join("/");
 
     return decodeURIComponent(`${platform() === "win32" ? "" : "/"}${path}`);
 }
