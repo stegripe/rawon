d17772ad28a1c2c61e5464a403cb2a38628ef5ec
Mednoob <ahmadyaqdhan123@gmail.com>
2022-03-27 23:08:59 +0900

    feat(SongManager): add logger for value changes

--- src/utils/structures/SongManager.ts
+++ src/utils/structures/SongManager.ts
@@ -1,7 +1,10 @@
 import { Song, QueueSong } from "../../typings";
+import { Rawon } from "../../structures/Rawon";
 import { Collection, GuildMember, Snowflake, SnowflakeUtil } from "discord.js";
 
 export class SongManager extends Collection<Snowflake, QueueSong> {
+    public constructor(public readonly client: Rawon, public readonly guild: GuildMember["guild"]) { super(); }
+
     public addSong(song: Song, requester: GuildMember): Snowflake {
         const key = SnowflakeUtil.generate();
         const data: QueueSong = {
@@ -13,6 +16,16 @@ export class SongManager extends Collection<Snowflake, QueueSong> {
 
         this.set(key, data);
         return key;
+    }
+
+    public set(key: Snowflake, data: QueueSong): this {
+        this.client.debugLog.logData("info", "SONG_MANAGER", `New value added to ${this.guild.name}(${this.guild.id}) song manager. Key: ${key}`);
+        return super.set(key, data);
+    }
+
+    public delete(key: Snowflake): boolean {
+        this.client.debugLog.logData("info", "SONG_MANAGER", `Value ${key} deleted from ${this.guild.name}(${this.guild.id}) song manager.`);
+        return super.delete(key);
     }
 
     public sortByIndex(): this {
