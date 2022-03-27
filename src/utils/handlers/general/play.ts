c759bca272b994bab197caf6629e998783d80b3d
Mednoob <ahmadyaqdhan123@gmail.com>
2022-03-27 23:12:46 +0900

    feat(play): add debug logs

--- src/utils/handlers/general/play.ts
+++ src/utils/handlers/general/play.ts
@@ -26,18 +26,23 @@ export async function play(guild: Guild, nextSong?: string, wasIdle?: boolean): 
                         }, 3500);
                     });
             }, 60000);
+        queue.client.debugLog.logData("info", "PLAY_HANDLER", `Queue ended for ${guild.name}(${guild.id})`);
         return;
     }
 
     const resource = createAudioResource(await getStream(song.song.url), { inlineVolume: true, metadata: song });
 
+    queue.client.debugLog.logData("info", "PLAY_HANDLER", `Created audio resource for ${guild.name}(${guild.id})`);
+
     queue.connection?.subscribe(queue.player);
 
     async function playResource(): Promise<void> {
         if (guild.channels.cache.get(queue!.connection!.joinConfig.channelId!)?.type === "GUILD_STAGE_VOICE") {
-            const suppressed = await guild.me?.voice.setSuppressed(false).catch(err => ({ error: err }));
+            queue?.client.debugLog.logData("info", "PLAY_HANDLER", `Trying to be a speaker in ${guild.me?.voice.channel?.name ?? "Unknown"}(${guild.me?.voice.channel?.id ?? "ID UNKNOWN"}) in guild ${guild.name}(${guild.id})`);
+            const suppressed = await guild.me?.voice.setSuppressed(false).catch((err: Error) => ({ error: err }));
             if (suppressed && "error" in suppressed) {
-                queue?.player.emit("error", new AudioPlayerError(suppressed.error as Error, resource));
+                queue?.client.debugLog.logData("error", "PLAY_HANDLER", `Failed to be a speaker in ${guild.me?.voice.channel?.name ?? "Unknown"}(${guild.me?.voice.channel?.id ?? "ID UNKNOWN"}) in guild ${guild.name}(${guild.id}). Reason: ${suppressed.error.message}`);
+                queue?.player.emit("error", new AudioPlayerError(suppressed.error, resource));
                 return;
             }
         }
@@ -48,12 +53,14 @@ export async function play(guild: Guild, nextSong?: string, wasIdle?: boolean): 
     if (wasIdle) {
         void playResource();
     } else {
+        queue.client.debugLog.logData("info", "PLAY_HANDLER", `Trying to enter Ready state in guild ${guild.name}(${guild.id}) voice connection`);
         entersState(queue.connection!, VoiceConnectionStatus.Ready, 15000)
             .then(async () => {
                 await playResource();
             })
             .catch((err: Error) => {
                 if (err.message === "The operation was aborted") err.message = "Cannot establish a voice connection within 15 seconds.";
+                queue.client.debugLog.logData("error", "PLAY_HANDLER", `Failed to enter Ready state in guild ${guild.name}(${guild.id}) voice connection. Reason: ${err.message}`);
                 queue.player.emit("error", new AudioPlayerError(err, resource));
             });
     }
