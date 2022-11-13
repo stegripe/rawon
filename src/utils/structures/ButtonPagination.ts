573bd27136ad5943fdccf9382e5afeef46f86929
godofwarOP <pratik11299gahane@gmail.com>
2022-11-13 18:54:37 +0530

    Fixed ButtonPagination (only method names are changed) filter need to be fixed

--- src/utils/structures/ButtonPagination.ts
+++ src/utils/structures/ButtonPagination.ts
@@ -1,41 +1,49 @@
 import {
     CommandInteraction,
-    ContextMenuInteraction,
+    ContextMenuCommandInteraction,
     Interaction,
-    InteractionButtonOptions,
     Message,
-    MessageActionRow,
-    MessageButton,
+    ActionRowBuilder,
+    ButtonBuilder,
     SelectMenuInteraction,
-    TextChannel
+    TextChannel,
+    ButtonStyle,
+    ComponentType,
+    InteractionButtonComponentData,
+    ButtonInteraction
 } from "discord.js";
 import { PaginationPayload } from "../../typings";
 
-const DATAS: InteractionButtonOptions[] = [
+const DATAS: InteractionButtonComponentData[] = [
     {
-        style: "SECONDARY",
+        style: ButtonStyle.Secondary,
         emoji: "⏪",
-        customId: "PREV10"
+        customId: `PREV10`,
+        type: ComponentType.Button
     },
     {
-        style: "PRIMARY",
+        style: ButtonStyle.Primary,
         emoji: "⬅️",
-        customId: "PREV"
+        customId: "PREV",
+        type: ComponentType.Button
     },
     {
-        style: "DANGER",
+        style: ButtonStyle.Danger,
         emoji: "🚫",
-        customId: "STOP"
+        customId: "STOP",
+        type: ComponentType.Button
     },
     {
-        style: "PRIMARY",
+        style: ButtonStyle.Primary,
         emoji: "➡️",
-        customId: "NEXT"
+        customId: "NEXT",
+        type: ComponentType.Button
     },
     {
-        style: "SECONDARY",
+        style: ButtonStyle.Secondary,
         emoji: "⏩",
-        customId: "NEXT10"
+        customId: "NEXT10",
+        type: ComponentType.Button
     }
 ];
 
@@ -43,7 +51,7 @@ export class ButtonPagination {
     public constructor(
         public readonly msg:
             | CommandInteraction
-            | ContextMenuInteraction
+            | ContextMenuCommandInteraction
             | Interaction
             | Message
             | SelectMenuInteraction,
@@ -58,11 +66,11 @@ export class ButtonPagination {
         this.payload.edit.call(this, index, embed, pages[index]);
 
         const isInteraction = this.msg instanceof CommandInteraction;
-        const buttons = DATAS.map(d => new MessageButton(d));
+        const buttons = DATAS.map(d => new ButtonBuilder(d));
         const toSend = {
             content: this.payload.content,
             embeds: [embed],
-            components: pages.length < 2 ? [] : [new MessageActionRow().addComponents(buttons)]
+            components: pages.length < 2 ? [] : [new ActionRowBuilder<ButtonBuilder>().addComponents(buttons)]
         };
         const msg = await (isInteraction
             ? (this.msg as CommandInteraction).editReply(toSend)
@@ -73,14 +81,22 @@ export class ButtonPagination {
 
         if (pages.length < 2) return;
 
+        /**
+         * @todo filter need to be fixed
+         */
+
+        const filter = (interaction: ButtonInteraction): any => {
+            void interaction.deferUpdate();
+            return (
+                DATAS.map(x => x.customId.toLowerCase()).includes(interaction.customId.toLowerCase()) &&
+                interaction.user.id === this.payload.author
+            );
+        };
+
+        this.msg.client.logger.debug(filter);
+
         const collector = fetchedMsg.createMessageComponentCollector({
-            filter: i => {
-                void i.deferUpdate();
-                return (
-                    DATAS.map(x => x.customId.toLowerCase()).includes(i.customId.toLowerCase()) &&
-                    i.user.id === this.payload.author
-                );
-            }
+            filter
         });
 
         collector.on("collect", async i => {
@@ -98,7 +114,7 @@ export class ButtonPagination {
                     index += 10;
                     break;
                 default:
-                    void (msg as Message).delete();
+                    void msg.delete();
                     return;
             }
 
@@ -108,7 +124,7 @@ export class ButtonPagination {
             await fetchedMsg.edit({
                 embeds: [embed],
                 content: this.payload.content,
-                components: pages.length < 2 ? [] : [new MessageActionRow().addComponents(buttons)]
+                components: pages.length < 2 ? [] : [new ActionRowBuilder<ButtonBuilder>().addComponents(buttons)]
             });
         });
     }
