de6f921cd569fb8170402b1fe10f40bacad0d5b0
Mednoob <ahmadyaqdhan123@gmail.com>
2023-01-12 22:40:35 +0900

    chore(searchTrack): forgot to remove console log

--- src/utils/handlers/general/searchTrack.ts
+++ src/utils/handlers/general/searchTrack.ts
@@ -80,7 +80,6 @@ export async function searchTrack(
                         const track = await youtube.getVideo(
                             /youtu\.be/g.exec(url.hostname) ? url.pathname.replace("/", "") : url.searchParams.get("v") ?? ''
                         );
-                        console.log("abc:", track);
 
                         if (track) {
                             result.items = [
