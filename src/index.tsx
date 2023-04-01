8823f9852e8bfef2071bbd8ff8c8de4c14a19b0f
noxzym <orchitiadi@gmail.com>
2023-04-01 19:30:25 +0700

    chore(RawonRoutes): add script route

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import Footer from "./components/Footer";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import { Suspense, lazy, ComponentProps } from "react";
 import ReactDOM from "react-dom";
+import Script from "./script";
 
 const Index = lazy(() => import("./mainpage"));
 const PermsCalculator = lazy(() => import("./permscalculator"));
@@ -12,7 +13,35 @@ const EmbedVisualizer = lazy(() => import("./embedvisualizer"));
 // Amogus
 function SusPense(props: ComponentProps<"div">): JSX.Element {
     return (
-        <Suspense fallback={<div className="flex justify-center items-center min-w-full h-screen text-base md:text-xl font-bold"><div className="m-2"><svg className="animate-spin mr-3 h-5 w-5 text-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle><path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg></div><p>Loading...</p></div>}>
+        <Suspense
+            fallback={
+                <div className="flex justify-center items-center min-w-full h-screen text-base md:text-xl font-bold">
+                    <div className="m-2">
+                        <svg
+                            className="animate-spin mr-3 h-5 w-5 text-black"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                        >
+                            <circle
+                                className="opacity-25"
+                                cx="12"
+                                cy="12"
+                                r="10"
+                                stroke="currentColor"
+                                stroke-width="4"
+                            ></circle>
+                            <path
+                                className="opacity-75"
+                                fill="currentColor"
+                                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                            ></path>
+                        </svg>
+                    </div>
+                    <p>Loading...</p>
+                </div>
+            }
+        >
             {props.children}
         </Suspense>
     );
@@ -23,11 +52,40 @@ function RawonRoutes(): JSX.Element {
         <HashRouter>
             {/* <Navbar/> */}
             <Routes>
-                <Route path="/" element={<SusPense><Index /></SusPense>}/>
-                <Route path="/permscalculator" element={<SusPense><PermsCalculator /></SusPense>}/>
-                <Route path="/embedvisualizer" element={<SusPense><EmbedVisualizer /></SusPense>}/>
+                <Route
+                    path="/"
+                    element={
+                        <SusPense>
+                            <Index />
+                        </SusPense>
+                    }
+                />
+                <Route
+                    path="/permscalculator"
+                    element={
+                        <SusPense>
+                            <PermsCalculator />
+                        </SusPense>
+                    }
+                />
+                <Route
+                    path="/embedvisualizer"
+                    element={
+                        <SusPense>
+                            <EmbedVisualizer />
+                        </SusPense>
+                    }
+                />
+                <Route
+                    path="/script"
+                    element={
+                        <SusPense>
+                            <Script />
+                        </SusPense>
+                    }
+                />
             </Routes>
-            <Footer/>
+            <Footer />
         </HashRouter>
     );
 }
