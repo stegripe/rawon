e45227a2ccac1b9400e0fb2ade6192924004defa
noxzym <orchitiadi@gmail.com>
2023-05-31 19:29:04 +0700

    chore(pages): add landing page

---
+++ src/pages/index.tsx
@@ -0,0 +1,58 @@
+import { Button, Container, Typography } from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function HomePage() {
+    return (
+        <>
+            <Container fixed className="flex h-screen w-full px-5 py-0 pt-0">
+                <div className="flex h-full w-full flex-col items-center justify-center gap-10">
+                    <div className="flex w-full flex-col items-center gap-4">
+                        <div className="relative aspect-square h-auto w-56">
+                            <Image
+                                src="/icons/icon-512x512.png"
+                                fill
+                                alt="rawon.jpg"
+                            />
+                        </div>
+                        <div className="flex w-full flex-col gap-4">
+                            <Typography className="text-center font-sans text-3xl font-medium uppercase text-third">
+                                rawon
+                            </Typography>
+                            <Typography className="text-center font-sans text-xl font-medium text-third">
+                                A simple powerful Discord music bot built to
+                                fulfill your production desires.
+                            </Typography>
+                        </div>
+                    </div>
+                    <div className="flex w-full flex-col items-center gap-4">
+                        <Link
+                            href="https://discord.com/api/oauth2/authorize?client_id=999162626036740138&permissions=275183430727&scope=applications.commands%20bot"
+                            className="h-9 w-48 rounded-lg text-inherit no-underline"
+                        >
+                            <Button
+                                id="inviteButton"
+                                color="inherit"
+                                className="h-full w-full rounded-lg bg-secondary font-sans text-lg font-semibold text-white"
+                            >
+                                invite
+                            </Button>
+                        </Link>
+                        <Link
+                            href="/discord"
+                            className="h-9 w-48 rounded-lg text-inherit no-underline"
+                        >
+                            <Button
+                                id="supportButton"
+                                color="inherit"
+                                className="h-full w-full rounded-lg border-2 border-solid border-secondary font-sans text-lg font-semibold text-secondary"
+                            >
+                                support
+                            </Button>
+                        </Link>
+                    </div>
+                </div>
+            </Container>
+        </>
+    );
+}
