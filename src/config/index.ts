0b067bfcd38eda2229c6ce2f818d357500df0adb
RomaDevWorld <50528338+RomaDevWorld@users.noreply.github.com>
2023-08-25 05:54:27 +0000

    update: src/config/index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -40,7 +40,7 @@ export const clientOptions: ClientOptions = {
 i18n.configure({
     defaultLocale: "en",
     directory: join(process.cwd(), "lang"),
-    locales: ["en", "es", "id", "fr", "zh-CN", "zh-TW"],
+    locales: ["en", "es", "id", "fr", "zh-CN", "zh-TW", "uk"],
     objectNotation: true
 });
 
