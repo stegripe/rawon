434df95ff7f29e75a6b4f91d3b24d946d30c0bb3
Mednoob <ahmadyaqdhan123@gmail.com>
2023-08-30 00:37:54 +0900

    chore(JSONDataManager): add `node:` to node import

--- src/utils/structures/JSONDataManager.ts
+++ src/utils/structures/JSONDataManager.ts
@@ -1,5 +1,5 @@
 import { OperationManager } from "./OperationManager.js";
-import { readFile, writeFile } from "fs/promises";
+import { readFile, writeFile } from "node:fs/promises";
 
 export class JSONDataManager<T> {
     private readonly manager = new OperationManager();
