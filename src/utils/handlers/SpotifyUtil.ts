7fb5657a1b10dade7c1634e5ba4ff7e085fe07d4
Fabrizio <81576840+PixlGalaxy@users.noreply.github.com>
2024-12-10 12:50:35 -0500

    Update SpotifyUtil.ts
    Resolved "This URL doesn't have any song data" issue caused by a malfunctioning Spotify token renewal timer. Improved timer logic to ensure proper token fetching and renewal, and implemented logging.

--- src/utils/handlers/SpotifyUtil.ts
+++ src/utils/handlers/SpotifyUtil.ts
@@ -4,50 +4,72 @@ import type { Rawon } from "../../structures/Rawon.js";
 import type { SpotifyAccessTokenAPIResult, SpotifyAlbum, SpotifyPlaylist, SpotifyTrack } from "../../typings/index.js";
 
 export class SpotifyUtil {
-    // eslint-disable-next-line prefer-named-capture-group
-    public spotifyRegex = /(?:https:\/\/open\.spotify\.com\/|spotify:)(?:.+)?(track|playlist|album)[/:]([\dA-Za-z]+)/u;
+    public spotifyRegex = /(?:https:\/\/open\.spotify\.com\/|spotify:)(?:.+)?(?<type>track|playlist|album)[/:](?<id>[\dA-Za-z]+)/u;
     public baseURI = "https://api.spotify.com/v1";
     private token!: string;
 
     public constructor(public client: Rawon) { }
 
-    public async fetchToken(): Promise<number> {
-        const { accessToken, accessTokenExpirationTimestampMs } = await this.client.request
-            .get("https://open.spotify.com/get_access_token", {
-                headers: {
-                    "User-Agent":
-                        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.59"
+    public async fetchTokenWithRetries(retries: number = 3): Promise<number> {
+
+        for (let attempt = 1; attempt <= retries; attempt++) {
+            try {
+                const { accessToken, accessTokenExpirationTimestampMs } = await this.client.request
+                    .get("https://open.spotify.com/get_access_token", {
+                        headers: {
+                            "User-Agent":
+                                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.59"
+                        }
+                    })
+                    .json<SpotifyAccessTokenAPIResult>();
+                if (accessToken === null || accessToken === undefined || accessToken.length === 0) {
+                    throw new Error("Could not fetch Spotify token.");
                 }
-            })
-            .json<SpotifyAccessTokenAPIResult>();
-        if ((accessToken?.length ?? 0) === 0) throw new Error("Could not fetch self Spotify token.");
-        this.token = `Bearer ${accessToken}`;
-        return new Date(accessTokenExpirationTimestampMs).getMilliseconds() * 1_000;
+                this.token = `Bearer ${accessToken}`;
+                return accessTokenExpirationTimestampMs - Date.now() - 300_000; // 5 minutos en milisegundos
+            } 
+            
+            catch (error) {
+                if (attempt === retries) {
+                    this.client.logger.error(`[SpotifyUtil] Failed to fetch Spotify token after ${retries} attempts: `, error);
+                    throw error;
+                }
+                this.client.logger.warn(`[SpotifyUtil] Attempt ${attempt} failed. Retrying...`);
+            }
+        }
+        throw new Error("Failed to fetch Spotify token after retries.");
     }
 
     public async renew(): Promise<void> {
-        const lastRenew = await this.fetchToken();
-        setTimeout(async () => this.renew(), lastRenew);
-    }
+        try {
+            const renewInterval = await this.fetchTokenWithRetries();
+            this.client.logger.info(`[SpotifyUtil] Token fetched successfully. Renewing in ${(renewInterval / 1_000 / 60).toFixed(2)} minutes.`);
+            setTimeout(async () => this.renew(), renewInterval);
+        }   
+        catch (error) {
+            this.client.logger.error("[SpotifyUtil] Failed to renew Spotify token: ", error);
+        }
+    }    
 
     public resolveTracks(url: string): Promise<{ track: SpotifyTrack }[]> | Promise<SpotifyTrack> | undefined {
+
         const [, type, id] = this.spotifyRegex.exec(url) ?? [];
+
         switch (type) {
-            case "track": {
+            
+            case "track":
                 return this.getTrack(id);
-            }
 
-            case "playlist": {
+            case "playlist":
                 return this.getPlaylist(id);
-            }
 
-            case "album": {
+            case "album":
                 return this.getAlbum(id);
-            }
 
-            default: break;
+            default:
+                break;
+                
         }
-
         return undefined;
     }
 
@@ -59,10 +81,12 @@ export class SpotifyUtil {
                 }
             })
             .json<SpotifyAlbum>();
+    
         let next = albumResponse.tracks.next;
-        while ((next?.length ?? 0) > 0) {
+
+        while (next !== null && next !== undefined) {
             const nextPlaylistResponse = await this.client.request
-                .get(next as unknown as string, {
+                .get(next, {
                     headers: {
                         Authorization: this.token
                     }
@@ -82,21 +106,34 @@ export class SpotifyUtil {
                 }
             })
             .json<SpotifyPlaylist>();
+        
+        const allTracks = playlistResponse.tracks.items.map((item, index) => ({
+            ...item,
+            originalIndex: index,
+        }));
+    
         let next = playlistResponse.tracks.next;
-        while ((next?.length ?? 0) > 0) {
+
+        while (next !== null && next !== undefined) {
             const nextPlaylistResponse = await this.client.request
-                .get(next as unknown as string, {
+                .get(next, {
                     headers: {
                         Authorization: this.token
                     }
                 })
                 .json<SpotifyPlaylist["tracks"]>();
+    
+            for (const [index, item] of nextPlaylistResponse.items.entries()) {
+                allTracks.push({
+                    ...item,
+                    originalIndex: allTracks.length + index,
+                });
+            }     
             next = nextPlaylistResponse.next;
-            playlistResponse.tracks.items.push(...nextPlaylistResponse.items);
         }
-
-        return playlistResponse.tracks.items.filter(spotifyTrack => spotifyTrack.track);
-    }
+        allTracks.sort((a, b) => a.originalIndex - b.originalIndex); 
+        return allTracks.map(({ track }) => ({ track }));
+    }    
 
     public async getTrack(id: string): Promise<SpotifyTrack> {
         return this.client.request
