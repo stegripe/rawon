8424eb0a20dbc122762108fcb902ea86c8465516
Mozart Rafi Kurniawan Tampubolon <mozartrafi@stegripe.org>
2025-08-16 23:27:02 +0700

    feat: Update dependencies, improve YTDLUtil, enhance Spotify integration, and update @discordjs/voice to 0.18.0 to support new encryption modes (#1473)
    * chore(env): rename example file
    
    * chore(lang/id): change mentions
    
    * fix(SkipToCommand): bug when using slash command
    
    * chore(config/env): update layout and prepare to implent spotify developer API
    
    * chore(config): implemented enablePrefix
    
    * fix(formatMS): time format for about command
    
    * fix(SpotifyUtil): implented spotify developer API
    
    * chore(YTDLUtil): handle improve
    
    * chore(deps): bump @discordjs/voice from 0.17.0 to 0.18.0, add ffmpeg-static
    
    * chore(deps-dev): bump @eslint/compat from 1.1.1 to 1.2.2, eslint from 9.10.0 to 9.14.0, typescript from 5.6.2 to 5.6.3
    
    * docs(readme): pin this amazing dev

--- src/config/env.ts
+++ src/config/env.ts
@@ -27,29 +27,32 @@ const formatLocale = (locale: string | undefined): string => {
     }
     return parts.join('-');
 };
+export const clientId = process.env.SPOTIFY_CLIENT_ID ?? "";
+export const clientSecret = process.env.SPOTIFY_CLIENT_SECRET ?? "";
 
-export const stayInVCAfterFinished = process.env.STAY_IN_VC_AFTER_FINISHED?.toLowerCase() === "yes";
+export const embedColor = (process.env.EMBED_COLOR?.toUpperCase() ?? "") || "00AD95";
+export const lang = formatLocale(process.env.LOCALE) || "en";
+export const mainGuild = parseEnvValue(process.env.MAIN_GUILD ?? "");
+export const streamStrategy = (process.env.STREAM_STRATEGY ?? "") || "yt-dlp";
+export const enablePrefix = process.env.ENABLE_PREFIX?.toLowerCase() !== "no";
 export const enableSlashCommand = process.env.ENABLE_SLASH_COMMAND?.toLowerCase() !== "no";
 export const is247Allowed = process.env.ENABLE_24_7_COMMAND?.toLowerCase() === "yes";
-export const isDev = process.env.NODE_ENV?.toLowerCase() === "development";
-export const debugMode = process.env.DEBUG_MODE?.toLowerCase() === "yes";
-export const enableRepl = process.env.REPL?.toLowerCase() === "yes";
-export const isProd = !isDev;
-
+export const stayInVCAfterFinished = process.env.STAY_IN_VC_AFTER_FINISHED?.toLowerCase() === "yes";
 export const musicSelectionType = (process.env.MUSIC_SELECTION_TYPE?.toLowerCase() ?? "") || "message";
-export const embedColor = (process.env.EMBED_COLOR?.toUpperCase() ?? "") || "00AD95";
 export const defaultVolume = Number(process.env.DEFAULT_VOLUME ?? 100) || 100;
-export const mainPrefix = isDev ? "d!" : (process.env.MAIN_PREFIX ?? "") || "!";
-export const streamStrategy = (process.env.STREAM_STRATEGY ?? "") || "yt-dlp";
-export const lang = formatLocale(process.env.LOCALE) || "en";
 export const yesEmoji = (process.env.YES_EMOJI ?? "") || "✅";
 export const noEmoji = (process.env.NO_EMOJI ?? "") || "❌";
+
+export const devs: string[] = parseEnvValue(process.env.DEVS ?? "");
+export const isDev = process.env.NODE_ENV?.toLowerCase() === "development";
+export const isProd = !isDev;
+export const mainPrefix = isDev ? "d!" : (process.env.MAIN_PREFIX ?? "") || "!";
+export const debugMode = process.env.DEBUG_MODE?.toLowerCase() === "yes";
+export const enableRepl = process.env.REPL?.toLowerCase() === "yes";
 
 export const altPrefixes: string[] = parseEnvValue((process.env.ALT_PREFIX ?? "") || "{mention}").filter(
     (x, i, a) => a.indexOf(x) === i && x !== mainPrefix
 );
-export const devs: string[] = parseEnvValue(process.env.DEVS ?? "");
-export const mainGuild = parseEnvValue(process.env.MAIN_GUILD ?? "");
 export const presenceData: PresenceData = {
     activities: parseEnvValue(process.env.ACTIVITIES ?? "").map((x, i) => ({
         name: x,
